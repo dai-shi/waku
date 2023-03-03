d0340d3935324dfe808a060a18fec4c5ce51b407
daishi <daishi@axlight.com>
2023-03-04 08:37:49 +0900

    move to src

--- cli.js
+++ cli.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env npx tsx --conditions react-server
 
-// FIXME: Unfortunately, the user has to install tsx manulally. Help wanted.
+// FIXME: Unfortunately, the user has to install tsx manulally.
 
 const cmd = process.argv[process.argv.length - 1];
-import(`./scripts/${cmd}.ts`);
+import(`./src/${cmd}.ts`);
