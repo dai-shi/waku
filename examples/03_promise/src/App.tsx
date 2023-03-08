6474894496300b9b0969d15872935ab031d5a620
daishi <daishi@axlight.com>
2023-03-08 09:17:33 +0900

    add examples/03

---
+++ examples/03_promise/src/App.tsx
@@ -0,0 +1,18 @@
+import { Counter } from "./Counter.tsx";
+
+const App = ({ name = "Anonymous" }) => {
+  const delayedMessage = new Promise<string>((resolve) => {
+    setTimeout(() => {
+      resolve("Hello from the server!");
+    }, 3000);
+  });
+  return (
+    <div style={{ border: "3px red dashed", margin: "1em", padding: "1em" }}>
+      <h1>Hello {name}!!</h1>
+      <h3>This is a server component.</h3>
+      <Counter delayedMessage={delayedMessage} />
+    </div>
+  );
+};
+
+export default App;
---
+++ examples/03_promise/src/Counter.tsx
@@ -0,0 +1,25 @@
+/// <reference types="react/experimental" />
+
+"use client";
+
+import { use, useState } from "react";
+
+export const Counter = ({
+  delayedMessage,
+}: {
+  delayedMessage: Promise<string>;
+}) => {
+  const [count, setCount] = useState(0);
+  return (
+    <div style={{ border: "3px blue dashed", margin: "1em", padding: "1em" }}>
+      <p>Count: {count}</p>
+      <button onClick={() => setCount((c) => c + 1)}>Increment</button>
+      <h3>This is a client component.</h3>
+      <Message delayedMessage={delayedMessage} />
+    </div>
+  );
+};
+
+const Message = ({ delayedMessage }: { delayedMessage: Promise<string> }) => (
+  <p>{use(delayedMessage)}</p>
+);
