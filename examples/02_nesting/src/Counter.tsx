4630beebe421ebc65365637034d9b07407e95cb4
daishi <daishi@axlight.com>
2023-03-08 11:50:59 +0900

    use suspense and transition in examples/02

--- examples/02_nesting/src/Counter.tsx
+++ examples/02_nesting/src/Counter.tsx
@@ -2,24 +2,38 @@
 
 "use client";
 
-import { cache, use, useState } from "react";
+import { Suspense, cache, use, useState, useTransition } from "react";
 import type { ReactNode } from "react";
 import { createFromFetch } from "react-server-dom-webpack/client";
 
-const fetchInnerApp = cache(
-  (count: number): Promise<ReactNode> =>
-    createFromFetch(fetch(`/RSC/InnerApp?count=${count}`))
-);
-
 export const Counter = ({ enableInnerApp = false }) => {
   const [count, setCount] = useState(0);
-  const innerApp = enableInnerApp && use(fetchInnerApp(count));
+  const [isPending, startTransition] = useTransition();
+  const inc = () => {
+    startTransition(() => {
+      setCount((c) => c + 1);
+    });
+  };
   return (
     <div style={{ border: "3px blue dashed", margin: "1em", padding: "1em" }}>
       <p>Count: {count}</p>
-      <button onClick={() => setCount((c) => c + 1)}>Increment</button>
+      <button onClick={inc}>Increment</button>
+      {isPending && <span>Pending...</span>}
       <h3>This is a client component.</h3>
-      {innerApp}
+      {enableInnerApp && (
+        <Suspense fallback="Pending...">
+          <InnerApp count={count} />
+        </Suspense>
+      )}
     </div>
   );
 };
+
+const fetchInnerApp = cache(async (count: number): Promise<ReactNode> => {
+  await new Promise((r) => setTimeout(r, 1000)); // emulate slow network
+  return createFromFetch(fetch(`/RSC/InnerApp?count=${count}`));
+});
+
+const InnerApp = ({ count }: { count: number }) => (
+  <>{use(fetchInnerApp(count))}</>
+);
