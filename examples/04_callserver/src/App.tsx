fb7497f8159bd46317949974133bba2b3a51600f
daishi <daishi@axlight.com>
2023-04-06 11:39:46 +0900

    assert server function types

--- examples/04_callserver/src/App.tsx
+++ examples/04_callserver/src/App.tsx
@@ -1,12 +1,16 @@
 import { Counter } from "./Counter.js";
 import { greet } from "./funcs.js";
 
+type ServerFunction<T> = T extends (...args: infer A) => infer R
+  ? (...args: A) => Promise<R>
+  : never;
+
 const App = ({ name = "Anonymous" }) => {
   return (
     <div style={{ border: "3px red dashed", margin: "1em", padding: "1em" }}>
       <h1>Hello {name}!!</h1>
       <h3>This is a server component.</h3>
-      <Counter greet={greet} />
+      <Counter greet={greet as unknown as ServerFunction<typeof greet>} />
     </div>
   );
 };
