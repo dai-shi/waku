1aa0203ef2050e193b0599105ead0fef6bd5bc5b
daishi <daishi@axlight.com>
2023-05-02 15:27:57 +0900

    use workder exec args instead of node options

--- cli-dev.js
+++ cli-dev.js
@@ -6,10 +6,11 @@ const cmd = process.argv[2];
 const execArgv = [
   "--conditions",
   "react-server",
-  ...(cmd === "dev" ? ["--experimental-loader", "tsx"] : []),
+  "--experimental-loader",
+  "tsx",
   "--experimental-loader",
   "wakuwork/node-loader",
   "--experimental-loader",
   "react-server-dom-webpack/node-loader",
 ];
-new Worker(`./dist/cli-${cmd}.js`, { execArgv });
+new Worker(`./src/cli-${cmd}.js`, { execArgv });
