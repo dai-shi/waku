9c124b68121d7a1e125f514e3d6c0d0217cace68
daishi <daishi@axlight.com>
2023-05-02 14:37:03 +0900

    Revert "remove S option for env"
    This reverts commit 61c6e262a429109b45fcb7258d8df86ec4f18b28.

--- cli.js
+++ cli.js
@@ -1,4 +1,4 @@
-#!/usr/bin/env node --conditions react-server --experimental-loader wakuwork/node-loader --experimental-loader react-server-dom-webpack/node-loader
+#!/usr/bin/env -S node --conditions react-server --experimental-loader wakuwork/node-loader --experimental-loader react-server-dom-webpack/node-loader
 
 const cmd = process.argv[2];
 import(`./dist/cli-${cmd}.js`);
