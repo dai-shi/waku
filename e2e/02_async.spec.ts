5987d502fa5f72032e54377c357e1733fbf13d64
himself65 <himself65@outlook.com>
2023-05-09 01:56:49 -0500

    fix: test

--- e2e/02_async.spec.ts
+++ e2e/02_async.spec.ts
@@ -2,9 +2,9 @@ import { test, expect } from "@playwright/test";
 
 test("async", async ({ page }) => {
     await page.goto('http://localhost:3001')
-    expect(await page.getByText('Pending...').isVisible()).toBeTruthy()
+    expect(page.getByText('Pending...').isVisible()).toBeTruthy()
     await page.waitForTimeout(500)
-    expect(await page.getByText('Pending...').isVisible()).toBeTruthy()
+    expect(page.getByText('Pending...').isVisible()).toBeTruthy()
     await page.waitForTimeout(550)
     // after 1 second, the server should have responded
     expect(await page.getByText('Hello from server!').isVisible()).toBeTruthy()
