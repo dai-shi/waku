cb08583b59d4515c2536791890f33699698cd211
himself65 <himself65@outlook.com>
2023-05-09 17:25:43 -0500

    fix: lint

--- e2e/02_async.spec.ts
+++ e2e/02_async.spec.ts
@@ -1,11 +1,11 @@
-import { test, expect } from "@playwright/test";
+import { test, expect } from '@playwright/test'
 
-test("async", async ({ page }) => {
-    await page.goto('http://localhost:3001')
-    expect(page.getByText('Pending...').isVisible()).toBeTruthy()
-    await page.waitForTimeout(500)
-    expect(page.getByText('Pending...').isVisible()).toBeTruthy()
-    await page.waitForTimeout(550)
-    // after 1 second, the server should have responded
-    expect(await page.getByText('Hello from server!').isVisible()).toBeTruthy()
+test('async', async ({ page }) => {
+  await page.goto('http://localhost:3001')
+  expect(page.getByText('Pending...').isVisible()).toBeTruthy()
+  await page.waitForTimeout(500)
+  expect(page.getByText('Pending...').isVisible()).toBeTruthy()
+  await page.waitForTimeout(550)
+  // after 1 second, the server should have responded
+  expect(await page.getByText('Hello from server!').isVisible()).toBeTruthy()
 })
