5115df4d0a5f0cb080f7290bbf33dc404d675abf
Himself65 <himself65@outlook.com>
2023-05-10 19:23:41 +0800

    test: add coverage for pending state (#34)
    * test: add coverage for pending state
    
    * fix: file
    
    * fix: test
    
    * ci: upload failed video
    
    * Update .github/workflows/ci.yml
    
    * fix: lint
    
    * feat: update config
    
    * fix: config
    
    * fix: revert code
    
    * ci: update
    
    * ci: yml
    
    * fix: ci
    
    * fix: ci
    
    ---------
    
    Co-authored-by: Daishi Kato <dai-shi@users.noreply.github.com>

--- e2e/01/counter.spec.ts
+++ e2e/01/counter.spec.ts
@@ -8,3 +8,12 @@ test('counter', async ({ page }) => {
   await page.click('text=Increment')
   expect(await page.locator('text=Count: 3').isVisible()).toBeTruthy()
 })
+
+test('first render', async ({ browser }) => {
+  const context = await browser.newContext({
+    javaScriptEnabled: false
+  });
+  const page = await context.newPage();
+  await page.goto('http://localhost:3000')
+  expect(await page.locator('.spinner').isVisible()).toBeTruthy()
+})
