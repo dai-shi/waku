8bd3dcd15a25ab91dd03e563f04a1dd4c6fc95ab
Daishi Kato <dai-shi@users.noreply.github.com>
2023-05-22 13:57:05 +0900

    update dependecies (#38)
    * update dependecies
    
    * increase e2e action timeout
    
    * adjust e2e timeout
    
    * adjust e2e timeout again
    
    * wip
    
    * Revert "wip"
    
    This reverts commit 1dc5477be5bbebc00e8d42883d49a039afdc26f2.

--- e2e/02/async.spec.ts
+++ e2e/02/async.spec.ts
@@ -3,9 +3,9 @@ import { test, expect } from '@playwright/test'
 test('async', async ({ page }) => {
   await page.goto('http://localhost:3000')
   expect(page.getByText('Pending...').isVisible()).toBeTruthy()
-  await page.waitForTimeout(500)
+  await page.waitForTimeout(200)
   expect(page.getByText('Pending...').isVisible()).toBeTruthy()
-  await page.waitForTimeout(550)
+  await page.waitForTimeout(1000)
   // after 1 second, the server should have responded
   expect(await page.getByText('Hello from server!').isVisible()).toBeTruthy()
 })
--- playwright.config.base.ts
+++ playwright.config.base.ts
@@ -1,7 +1,7 @@
 import type {
   PlaywrightTestConfig,
   PlaywrightWorkerOptions,
-} from '@playwright/test';
+} from "@playwright/test";
 // import { devices } from '@playwright/test';
 
 /**
@@ -15,21 +15,24 @@ import type {
  */
 export const config: PlaywrightTestConfig = {
   fullyParallel: true,
-  timeout: process.env.CI ? 50_000 : 30_000,
+  timeout: process.env.CI ? 60_000 : 30_000,
+  expect: {
+    timeout: process.env.CI ? 10_000 : 5_000,
+  },
   use: {
-    baseURL: 'http://localhost:3000/',
+    baseURL: "http://localhost:3000/",
     browserName:
-      (process.env.BROWSER as PlaywrightWorkerOptions['browserName']) ??
-      'chromium',
+      (process.env.BROWSER as PlaywrightWorkerOptions["browserName"]) ??
+      "chromium",
     viewport: { width: 1440, height: 800 },
-    actionTimeout: 5 * 1000,
-    locale: 'en-US',
+    actionTimeout: 5_000,
+    locale: "en-US",
     // Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer
     // You can open traces locally(`npx playwright show-trace trace.zip`)
     // or in your browser on [Playwright Trace Viewer](https://trace.playwright.dev/).
-    trace: 'on-first-retry',
+    trace: "on-first-retry",
     // Record video only when retrying a test for the first time.
-    video: 'on-first-retry',
+    video: "on-first-retry",
   },
   forbidOnly: !!process.env.CI,
   workers: 4,
@@ -37,12 +40,12 @@ export const config: PlaywrightTestConfig = {
   // 'github' for GitHub Actions CI to generate annotations, plus a concise 'dot'
   // default 'list' when running locally
   // See https://playwright.dev/docs/test-reporters#github-actions-annotations
-  reporter: process.env.CI ? 'github' : 'list',
+  reporter: process.env.CI ? "github" : "list",
 };
 
 if (process.env.CI) {
   config.retries = 3;
-  config.workers = '50%';
+  config.workers = "50%";
 }
 
 export default config;
