1cb798cbc9dfef22c80476c89c0600ff07eb5c1b
daishi <daishi@axlight.com>
2023-05-22 23:03:25 +0900

    wakuwork->waku

--- examples/99_jotai/src/index.tsx
+++ examples/99_jotai/src/index.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { serve } from "wakuwork/client";
+import { serve } from "waku/client";
 
 import { ClientProvider, useAtomValues } from "../lib/jotai-rsc/client.js";
 
@@ -9,7 +9,7 @@ const root = createRoot(document.getElementById("root")!);
 const App = serve<{ name: string; atomValues: unknown }>("App");
 const Main = () => {
   const atomValues = useAtomValues();
-  return <App name="Wakuwork" atomValues={atomValues} />;
+  return <App name="Waku" atomValues={atomValues} />;
 };
 root.render(
   <StrictMode>
