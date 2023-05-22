8b8a14c32686fd98930013cb66d7b102d1c2187b
daishi <daishi@axlight.com>
2023-05-22 15:51:54 +0900

    fix entries

--- examples/99_jotai/entries.ts
+++ examples/99_jotai/entries.ts
@@ -1,29 +1,21 @@
-import type { GetEntry, Prefetcher, Prerenderer } from "wakuwork/server";
+import { defineEntries } from "wakuwork/server";
 
-export const getEntry: GetEntry = async (id) => {
-  switch (id) {
-    case "App":
-      return import("./src/App.js");
-    default:
-      throw new Error(`Unknown entry id: ${id}`);
+export default defineEntries(
+  // getEntry
+  async (id) => {
+    switch (id) {
+      case "App":
+        return import("./src/App.js");
+      default:
+        return null;
+    }
+  },
+  // getBuilder
+  async () => {
+    return {
+      "/": {
+        elements: [["App", { name: "Wakuwork" }]],
+      },
+    };
   }
-};
-
-export const prefetcher: Prefetcher = async (path) => {
-  switch (path) {
-    case "/":
-      return {
-        entryItems: [["App", { name: "Wakuwork" }]],
-        clientModules: [(await import("./src/Counter.js")).Counter],
-      };
-    default:
-      return {};
-  }
-};
-
-export const prerenderer: Prerenderer = async () => {
-  return {
-    entryItems: [["App", { name: "Wakuwork" }]],
-    paths: ["/"],
-  };
-};
+);
