6f3971035f9b51f656d7bbfd3a6e99c3e102f066
daishi <daishi@axlight.com>
2023-05-23 21:22:27 +0900

    remove unused files

--- src/cli-build.ts
+++
@@ -1,7 +0,0 @@
-import { build } from "./builder.js";
-
-const runBuild = (_config: unknown) => build();
-
-const config = process.env.WAKU_CONFIG && JSON.parse(process.env.WAKU_CONFIG);
-
-await runBuild(config);
