908a2215aaa792eb1653549752a6f1501ecfba1c
himself65 <himself65@outlook.com>
2023-06-04 01:07:34 +0800

    fix: document overflow

--- website/routes/index.tsx
+++ website/routes/index.tsx
@@ -72,11 +72,11 @@ const Home = () => (
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <div className="flex flex-col-reverse sm:flex-row">
+    <div className="flex flex-col-reverse sm:flex-row h-screen">
       <nav className="w-full sm:w-64">
         <Sidebar />
       </nav>
-      <main className="flex-1 p-6 w-full sm:w-[calc(100%-16rem)]">
+      <main className="flex-1 p-6 w-full sm:w-[calc(100%-16rem)] overflow-scroll">
         {children || <Home />}
       </main>
     </div>
