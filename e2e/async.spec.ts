31be06b73846682da4869869b4e0383a680e5606
daishi <daishi@axlight.com>
2023-06-04 16:01:00 +0900

    add prettier

--- e2e/async.spec.ts
+++ e2e/async.spec.ts
@@ -1,12 +1,12 @@
 // 02_async
-import { test, expect } from '@playwright/test'
+import { test, expect } from "@playwright/test";
 
-test('async', async ({ page }) => {
-  await page.goto('http://localhost:3001')
-  await expect(page.getByText('Pending...')).toBeVisible()
-  await page.waitForTimeout(500)
-  await expect(page.getByText('Pending...')).toBeVisible()
-  await page.waitForTimeout(550)
+test("async", async ({ page }) => {
+  await page.goto("http://localhost:3001");
+  await expect(page.getByText("Pending...")).toBeVisible();
+  await page.waitForTimeout(500);
+  await expect(page.getByText("Pending...")).toBeVisible();
+  await page.waitForTimeout(550);
   // after 1 second, the server should have responded
-  await expect(page.getByText('Hello from server!')).toBeVisible()
-})
+  await expect(page.getByText("Hello from server!")).toBeVisible();
+});
--- e2e/counter.spec.ts
+++ e2e/counter.spec.ts
@@ -1,20 +1,20 @@
 // 01_counter
-import { test, expect } from '@playwright/test'
+import { test, expect } from "@playwright/test";
 
-test('counter', async ({ page }) => {
-  await page.goto('http://localhost:3000')
-  await expect(page.locator('text=Count: 0')).toBeVisible()
-  await page.click('text=Increment')
-  await page.click('text=Increment')
-  await page.click('text=Increment')
-  await expect(page.locator('text=Count: 3')).toBeVisible()
-})
+test("counter", async ({ page }) => {
+  await page.goto("http://localhost:3000");
+  await expect(page.locator("text=Count: 0")).toBeVisible();
+  await page.click("text=Increment");
+  await page.click("text=Increment");
+  await page.click("text=Increment");
+  await expect(page.locator("text=Count: 3")).toBeVisible();
+});
 
-test('first render', async ({ browser }) => {
+test("first render", async ({ browser }) => {
   const context = await browser.newContext({
-    javaScriptEnabled: false
+    javaScriptEnabled: false,
   });
   const page = await context.newPage();
-  await page.goto('http://localhost:3000')
-  await expect(page.locator('.spinner')).toBeVisible()
-})
+  await page.goto("http://localhost:3000");
+  await expect(page.locator(".spinner")).toBeVisible();
+});
