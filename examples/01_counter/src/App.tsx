9fe52956535b2676d12bcf7471e599911b14faba
daishi <daishi@axlight.com>
2023-06-12 14:47:47 +0900

    fix typo

--- examples/01_counter/src/App.tsx
+++ examples/01_counter/src/App.tsx
@@ -1,13 +1,13 @@
 import { Suspense } from "react";
 import { Counter } from "./Counter.js";
-import { CounterSkelton } from "./CounterSkelton.js";
+import { CounterSkeleton } from "./CounterSkeleton.js";
 
 const App = ({ name = "Anonymous" }) => {
   return (
     <div style={{ border: "3px red dashed", margin: "1em", padding: "1em" }}>
       <h1>Hello {name}!!</h1>
       <h3>This is a server component.</h3>
-      <Suspense fallback={<CounterSkelton />}>
+      <Suspense fallback={<CounterSkeleton />}>
         <Counter />
       </Suspense>
     </div>
