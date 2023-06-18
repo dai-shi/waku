869dd85bb1feb299b8da3d761bd0177c63753a9f
daishi <daishi@axlight.com>
2023-06-18 09:48:08 +0900

    disable SSR for 02_async

--- examples/02_async/entries.ts
+++ examples/02_async/entries.ts
@@ -17,14 +17,15 @@ export default defineEntries(
         elements: [["App", { name: "Waku" }]],
       },
     };
-  },
-  // getSsrConfig
-  async (pathStr) => {
-    switch (pathStr) {
-      case "/":
-        return { element: ["App", { name: "Waku" }] };
-      default:
-        return null;
-    }
   }
+  // getSsrConfig
+  // FIXME SSR streaming and RSC streaming conflict. Let's revisit it later.
+  // async (pathStr) => {
+  //   switch (pathStr) {
+  //     case "/":
+  //       return { element: ["App", { name: "Waku" }] };
+  //     default:
+  //       return null;
+  //   }
+  // }
 );
