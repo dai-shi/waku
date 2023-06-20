96d7238bb456a9ce95db36cdb9aaf886354e716c
Ahmed K. A <ahmadkabdullah@pm.me>
2023-06-20 16:09:57 +0300

    redefine props for MyLink and make it take children

--- website/src/components/MyLink.tsx
+++ website/src/components/MyLink.tsx
@@ -1,16 +1,26 @@
 import { Link } from "waku/router/server";
 
-export const MyLink = ({ name, href }: { name: string; href: string }) => (
-  <Link
-    href={href}
-    pending={
-      <div className="animate-ping w-2 h-2 rounded-full bg-cCarmine"></div>
-    }
-    notPending={
-      <div className="w-2 h-2 rounded-full bg-transparent"></div>
-    }
-    unstable_prefetchOnEnter
-  >
-    {name}
-  </Link>
+export const MyLink = (props: {
+  href: string;
+  noPending?: boolean;
+  children: React.ReactNode;
+}) => (
+  <div className="flex flex-row gap-1 items-center">
+    <Link
+      href={props.href}
+      pending={
+        props.noPending ? undefined : (
+          <div className="animate-ping w-2 h-2 rounded-full bg-cCarmine"></div>
+        )
+      }
+      notPending={
+        props.noPending ? undefined : (
+          <div className="w-2 h-2 rounded-full bg-transparent"></div>
+        )
+      }
+      unstable_prefetchOnEnter
+    >
+      {props.children}
+    </Link>
+  </div>
 );
--- website/src/components/Nav.tsx
+++ website/src/components/Nav.tsx
@@ -1,15 +1,23 @@
+import { MyLink } from "./MyLink.js";
+
 export const Nav = () => {
   return (
     <nav className="px-8 py-4 flex gap-4 flex-row justify-between items-center sticky top-0 bg-cWhite/80 backdrop-blur-md">
       {/* logo */}
       <div>
-        <h1 className="font-bold text-xl font-mono">Waku</h1>
+        <MyLink href="/">
+          <h1 className="font-bold text-xl font-mono">Waku</h1>
+        </MyLink>
       </div>
 
       {/* pages */}
       <div className="flex flex-row gap-4 items-center font-bold">
-        <h1>Home</h1>
-        <h1>Docs</h1>
+        <MyLink href="/">
+          <h1>Home</h1>
+        </MyLink>
+        <MyLink href="/docs/">
+          <h1>Docs</h1>
+        </MyLink>
       </div>
     </nav>
   );
