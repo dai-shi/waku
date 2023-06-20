adb7827c2bca3f2fa6dd66d6c646544578471b32
Ahmed K. A <ahmadkabdullah@pm.me>
2023-06-20 16:02:46 +0300

    improve styling on Nav and make it sticky

--- website/src/components/Nav.tsx
+++ website/src/components/Nav.tsx
@@ -1,13 +1,13 @@
 export const Nav = () => {
   return (
-    <nav className="px-8 py-4 flex gap-4 flex-row justify-between items-center">
+    <nav className="px-8 py-4 flex gap-4 flex-row justify-between items-center sticky top-0 bg-cWhite/80 backdrop-blur-md">
       {/* logo */}
       <div>
-        <h1>Waku</h1>
+        <h1 className="font-bold text-xl font-mono">Waku</h1>
       </div>
 
       {/* pages */}
-      <div className="flex flex-row gap-4 items-center">
+      <div className="flex flex-row gap-4 items-center font-bold">
         <h1>Home</h1>
         <h1>Docs</h1>
       </div>
--- website/src/components/Sidebar.tsx
+++ website/src/components/Sidebar.tsx
@@ -1,7 +1,7 @@
 import { MyLink } from "./MyLink.js";
 
 export const Sidebar = () => (
-  <aside className="h-full sticky top-10">
+  <aside className="h-full sticky top-20">
     <ul className="flex flex-col gap-4">
       <li className="flex flex-row gap-2 items-center">
         <MyLink href="/docs/introduction" name="Introduction" />
