d1cd394934008d8e141211130f59e71f613d3a04
Ahmed K. A <ahmadkabdullah@pm.me>
2023-06-25 00:37:06 +0300

    Fix styling on landing page and footer

--- website/src/routes/index.tsx
+++ website/src/routes/index.tsx
@@ -3,32 +3,32 @@ import { Nav } from "../components/Nav.js";
 import { Footer } from "../components/Footer.js";
 
 const Home = () => (
-	<div className="flex flex-col gap-16 justify-center items-center h-full">
-		<div className="flex flex-col gap-0 text-4xl font-extrabold items-center">
-			<h1>The Minimalist</h1>
-			<h1 className="text-cCarmine">React Framework</h1>
-			<h1>With Server Components</h1>
-		</div>
+  <div className="grow flex flex-col gap-16 justify-center items-center">
+    <div className="flex flex-col gap-0 text-4xl font-extrabold items-center">
+      <h1>The Minimalist</h1>
+      <h1 className="text-cCarmine">React Framework</h1>
+      <h1>With Server Components</h1>
+    </div>
 
-		<div className="flex flex-row gap-4 text-xl">
-			<button className="rounded-full px-4 py-1 border border-cBlack text-cBlack">
-				Contribute
-			</button>
-			<button className="rounded-full px-4 py-1 border border-cBlack bg-cCarmine text-cWhite">
-				Get Started
-			</button>
-		</div>
+    <div className="flex flex-row gap-4 text-xl">
+      <button className="rounded-full px-4 py-1 border border-cBlack text-cBlack">
+        Contribute
+      </button>
+      <button className="rounded-full px-4 py-1 border border-cBlack bg-cCarmine text-cWhite">
+        Get Started
+      </button>
+    </div>
 
-		<div>Arrow</div>
-	</div>
+    <div>Arrow</div>
+  </div>
 );
 
 export default function Layout({ children }: { children: ReactNode }) {
-	return (
-		<div className="flex flex-col min-h-screen w-screen bg-cWhite text-cBlack gap-8">
-			<Nav></Nav>
-			<main className="grow px-8">{children || <Home />}</main>
-			<Footer></Footer>
-		</div>
-	);
+  return (
+    <div className="flex flex-col min-h-screen w-screen bg-cWhite text-cBlack gap-8">
+      <Nav></Nav>
+      <main className="grow px-8 flex">{children || <Home />}</main>
+      <Footer></Footer>
+    </div>
+  );
 }
