c6f1b00028c265233524bcecae97d9e0ee733b53
Ahmed K. A <ahmadkabdullah@pm.me>
2023-06-24 14:13:21 +0300

    Fix sidbar introduction link

--- website/src/components/Sidebar.tsx
+++ website/src/components/Sidebar.tsx
@@ -3,7 +3,11 @@ import { SidebarItem } from "./SidebarItem.js";
 
 export const Sidebar = () => (
 	<aside className="h-full sticky top-20 flex flex-col gap-2">
-		<SidebarItem selected={true} link="/docs" text="Introduction" />
+		<SidebarItem
+			selected={true}
+			link="/docs/introduction"
+			text="Introduction"
+		/>
 		<SidebarItem link="/docs/installing" text="Installing" />
 		<SidebarGroup text="Practices">
 			<SidebarItem link="/docs/practices/minimal" text="Minimal" />
