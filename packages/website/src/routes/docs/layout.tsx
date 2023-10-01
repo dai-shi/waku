11b153e197c3b7c2fa1b46d36b66578f561f1918
daishi <daishi@axlight.com>
2023-10-01 23:14:07 +0900

    website: show current date

--- packages/website/src/routes/docs/layout.tsx
+++ packages/website/src/routes/docs/layout.tsx
@@ -12,6 +12,7 @@ export default function Layout({
     <div className="flex flex-col md:flex-row gap-16 md:gap-8">
       <Sidebar path={path}></Sidebar>
       <div className="md:grow md:px-12">{children}</div>
+      <p>Current Date: {new Date().toISOString()}</p>
     </div>
   );
 }
