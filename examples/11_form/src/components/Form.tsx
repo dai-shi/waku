18147fa87691313753ff9373a58cbeb7bcbc7ff6
daishi <daishi@axlight.com>
2023-10-24 08:57:38 +0900

    ah, i see... this is confusing

--- examples/11_form/src/components/Form.tsx
+++ examples/11_form/src/components/Form.tsx
@@ -5,25 +5,26 @@
 // @ts-expect-error no exported member
 import { useFormStatus } from "react-dom";
 
+const FormStatus = () => {
+  const { pending } = useFormStatus();
+  return pending ? "Pending..." : null;
+};
+
 export const Form = ({
   message,
   greet,
 }: {
   message: string;
   greet: (formData: FormData) => Promise<void>;
-}) => {
-  // FIXME This doesn't seem to work as expected.
-  const { pending } = useFormStatus();
-  return (
-    <div style={{ border: "3px blue dashed", margin: "1em", padding: "1em" }}>
-      <p>{message}</p>
-      {/* @ts-expect-error not assignable to type 'string' */}
-      <form action={greet}>
-        Name: <input name="name" />
-        <input type="submit" value="Send" />
-        {pending && "Pending..."}
-      </form>
-      <h3>This is a client component.</h3>
-    </div>
-  );
-};
+}) => (
+  <div style={{ border: "3px blue dashed", margin: "1em", padding: "1em" }}>
+    <p>{message}</p>
+    {/* @ts-expect-error not assignable to type 'string' */}
+    <form action={greet}>
+      Name: <input name="name" />
+      <input type="submit" value="Send" />
+      <FormStatus />
+    </form>
+    <h3>This is a client component.</h3>
+  </div>
+);
