17a476123606f41d41924e3502bc4311848b39c6
Alex Yang <himself65@outlook.com>
2023-10-23 18:03:27 -0500

    refactor: use ts monorepo

--- packages/waku/src/client.ts
+++ packages/waku/src/client.ts
@@ -1,5 +1,3 @@
-/// <reference types="react/canary" />
-
 "use client";
 
 import {
--- packages/waku/src/types.d.ts
+++ packages/waku/src/types.d.ts
@@ -3,11 +3,3 @@ declare module "react-server-dom-webpack/server";
 declare module "react-server-dom-webpack/server.node.unbundled";
 declare module "react-server-dom-webpack/client";
 declare module "react-server-dom-webpack/client.node.unbundled";
-
-// FIXME Is this too naive? How can we avoid the eslint warning?
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-namespace JSX {
-  export type ElementType =
-    | string
-    | ((props: any) => React.ReactNode | Promise<React.ReactNode>);
-}
