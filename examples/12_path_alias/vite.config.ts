ab65815f47cbafba4bbc1bc88d0de004d9ce36e2
Alex Yang <himself65@outlook.com>
2023-10-29 19:28:23 -0500

    fix: code

--- examples/12_path_alias/vite.config.ts
+++ examples/12_path_alias/vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from "waku/config";
-import tsconfigPaths from 'vite-tsconfig-paths'
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
