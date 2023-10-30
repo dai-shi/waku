d051c360c714a29a4e65436141fe499f3112bc49
Alex Yang <himself65@outlook.com>
2023-10-29 19:28:03 -0500

    fix: code

--- examples/12_path_alias/vite.config.ts
+++ examples/12_path_alias/vite.config.ts
@@ -1,18 +1,6 @@
 import { defineConfig } from "waku/config";
 import tsconfigPaths from 'vite-tsconfig-paths'
 
-const getFallback = (id: string) => {
-  if (id.endsWith("#Counter")) {
-    return "waku/server#ClientOnly";
-  }
-  return "waku/server#ClientFallback";
-};
-
 export default defineConfig({
-  framework: {
-    ssr: {
-      getFallback,
-    },
-  },
   plugins: [tsconfigPaths()],
 });
