208d9b9445d44d16b08223fcf617d14922fc565c
daishi <daishi@axlight.com>
2023-10-30 14:37:41 +0900

    wip: hack to bypass server-only

--- packages/website/src/components/Code.tsx
+++ packages/website/src/components/Code.tsx
@@ -1,4 +1,11 @@
-import { Code as BrightCode, BrightProps } from "bright";
+import { Suspense, lazy } from "react";
+import type { BrightProps } from "bright";
+
+// import { Code as BrightCode } from "bright";
+// FIXME: For now, this is a hack to bypass server-only error with SSR.
+const BrightCode = lazy(async () => ({
+  default: (await import("bright")).Code,
+}));
 
 export const Code = ({ children }: { children: string }) => (
   <code className="text-cCarmine font-mono rounded-full px-1 py-1">
@@ -13,10 +20,12 @@ export const CodeBlock = ({
   children: string;
   lang: BrightProps["lang"];
 }) => (
-  <BrightCode
-    className="!p-0 !rounded-2xl overflow-scroll max-w-xs sm:max-w-sm md:max-w-md lg:max-w-full !m-0 border-2 border-cVanilla"
-    theme="solarized-light"
-    code={children}
-    lang={lang}
-  />
+  <Suspense>
+    <BrightCode
+      className="!p-0 !rounded-2xl overflow-scroll max-w-xs sm:max-w-sm md:max-w-md lg:max-w-full !m-0 border-2 border-cVanilla"
+      theme="solarized-light"
+      code={children}
+      lang={lang}
+    />
+  </Suspense>
 );
