ff56d3b92dd3d777385db557420c8b474441251d
daishi <daishi@axlight.com>
2023-10-30 21:03:51 +0900

    Revert "wip: hack to bypass server-only"
    This reverts commit 208d9b9445d44d16b08223fcf617d14922fc565c.

--- packages/website/src/components/Code.tsx
+++ packages/website/src/components/Code.tsx
@@ -1,11 +1,4 @@
-import { Suspense, lazy } from "react";
-import type { BrightProps } from "bright";
-
-// import { Code as BrightCode } from "bright";
-// FIXME: For now, this is a hack to bypass server-only error with SSR.
-const BrightCode = lazy(async () => ({
-  default: (await import("bright")).Code,
-}));
+import { Code as BrightCode, BrightProps } from "bright";
 
 export const Code = ({ children }: { children: string }) => (
   <code className="text-cCarmine font-mono rounded-full px-1 py-1">
@@ -20,12 +13,10 @@ export const CodeBlock = ({
   children: string;
   lang: BrightProps["lang"];
 }) => (
-  <Suspense>
-    <BrightCode
-      className="!p-0 !rounded-2xl overflow-scroll max-w-xs sm:max-w-sm md:max-w-md lg:max-w-full !m-0 border-2 border-cVanilla"
-      theme="solarized-light"
-      code={children}
-      lang={lang}
-    />
-  </Suspense>
+  <BrightCode
+    className="!p-0 !rounded-2xl overflow-scroll max-w-xs sm:max-w-sm md:max-w-md lg:max-w-full !m-0 border-2 border-cVanilla"
+    theme="solarized-light"
+    code={children}
+    lang={lang}
+  />
 );
