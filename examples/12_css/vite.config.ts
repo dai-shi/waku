1987dce0bbce91436fbd8ad3f347ef25678adb20
Mohammad Bagher Abiat <zorofight94@gmail.com>
2023-11-20 19:26:53 +0330

    Update vite.config.ts to include vanillaExtractPlugin

--- examples/12_css/vite.config.ts
+++ examples/12_css/vite.config.ts
@@ -3,6 +3,7 @@ import path from 'node:path'
 // import { defineConfig } from 'waku/config'
 import { defineConfig } from 'vite';
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin'
+
 export default defineConfig({
   root: path.dirname(url.fileURLToPath(import.meta.url)),
   plugins: [vanillaExtractPlugin({ emitCssInSsr: true })]
