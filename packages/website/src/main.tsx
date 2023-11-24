96b0758842e29742cc38501ef00fe7ceaea7a617
daishi <daishi@axlight.com>
2023-11-24 17:57:08 +0900

    fix website main.tsx

--- packages/website/src/main.tsx
+++ packages/website/src/main.tsx
@@ -10,17 +10,8 @@ const rootElement = (
   </StrictMode>
 );
 
-// FIXME temporary fix, doesn't feel ideal.
-function init() {
-  const root = document.getElementById('root');
-  if (!root) {
-    setTimeout(init);
-    return;
-  }
-  if ((globalThis as any).__WAKU_SSR_ENABLED__) {
-    hydrateRoot(root, rootElement);
-  } else {
-    createRoot(root).render(rootElement);
-  }
+if ((globalThis as any).__WAKU_SSR_ENABLED__) {
+  hydrateRoot(document.getElementById('root')!, rootElement);
+} else {
+  createRoot(document.getElementById('root')!).render(rootElement);
 }
-init();
