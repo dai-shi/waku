872588748795b8a160d86b73a843965b46bd3872
Daishi Kato <dai-shi@users.noreply.github.com>
2023-11-27 23:03:25 +0900

    switch to hono from express (still depends on node) (#165)
    * wip: experimental switch to hono
    
    * prettier
    
    * wip: some fixes
    
    * keep using express for examples 08
    
    * wip: some more fixes
    
    * url must be pathname + searhquery
    
    * get full headers

--- packages/waku/src/cli.ts
+++ packages/waku/src/cli.ts
@@ -3,8 +3,9 @@
 import path from 'node:path';
 import { parseArgs } from 'node:util';
 import { createRequire } from 'node:module';
-
-import type { Express } from 'express';
+import { PassThrough, Readable } from 'node:stream';
+import type { IncomingMessage, ServerResponse } from 'node:http';
+import type { Hono, MiddlewareHandler } from 'hono';
 
 const require = createRequire(new URL('.', import.meta.url));
 
@@ -53,11 +54,67 @@ if (values.version) {
   }
 }
 
+type Middleware = (
+  req: IncomingMessage,
+  res: ServerResponse,
+  next: (err?: unknown) => void,
+) => void;
+
+const wrap =
+  (m: Middleware): MiddlewareHandler =>
+  (c, next) =>
+    new Promise((resolve) => {
+      let req: any; // HACK
+      if (c.req.raw.body) {
+        req = Readable.fromWeb(c.req.raw.body as any);
+      } else {
+        req = new PassThrough();
+        req.end();
+      }
+      req.method = c.req.method;
+      // TODO we should support full URL string in our middleware
+      req.url = c.req.url.slice(new URL(c.req.url).origin.length);
+      req.headers = Object.fromEntries(
+        Array.from(c.req.raw.headers.entries()).map(([k, v]) => [k, v]),
+      );
+      const res = new PassThrough() as any; // HACK
+      const stream = Readable.toWeb(res) as any;
+      let resolved = false;
+      res.on('data', () => {
+        if (!resolved) {
+          resolved = true;
+          resolve(c.body(stream));
+        }
+      });
+      res.on('close', () => {
+        if (!resolved) {
+          resolved = true;
+          resolve(c.body(null));
+        }
+      });
+      Object.defineProperty(res, 'statusCode', {
+        set(code) {
+          c.status(code);
+        },
+      });
+      res.getHeader = (name: string) => c.res.headers.get(name);
+      res.setHeader = (name: string, value: string) => {
+        c.header(name, value);
+      };
+      res.writeHead = (code: number, headers?: Record<string, string>) => {
+        c.status(code);
+        for (const [name, value] of Object.entries(headers || {})) {
+          c.header(name, value);
+        }
+      };
+      m(req, res, () => next().then(resolve));
+    });
+
 async function runDev(options: { ssr: boolean }) {
-  const { default: express } = await import('express');
+  const { Hono } = await import('hono');
   const { rsc } = await import('./lib/middleware/rsc.js');
-  const app = express();
-  app.use(rsc({ command: 'dev', ssr: options.ssr }));
+  const app = new Hono();
+  app.use('*', wrap(rsc({ command: 'dev', ssr: options.ssr })));
   const port = parseInt(process.env.PORT || '3000', 10);
   startServer(app, port);
 }
@@ -68,22 +125,29 @@ async function runBuild(options: { ssr: boolean }) {
 }
 
 async function runStart(options: { ssr: boolean }) {
-  const { default: express } = await import('express');
+  const { Hono } = await import('hono');
+  const { serveStatic } = await import('@hono/node-server/serve-static');
   const { resolveConfig } = await import('./lib/config.js');
   const config = await resolveConfig();
   const { rsc } = await import('./lib/middleware/rsc.js');
-  const app = express();
-  app.use(rsc({ command: 'start', ssr: options.ssr }));
+  const app = new Hono();
+  app.use('*', wrap(rsc({ command: 'start', ssr: options.ssr })));
   app.use(
-    express.static(path.join(config.rootDir, config.distDir, config.publicDir)),
+    '*',
+    serveStatic({
+      root: path.relative(
+        path.resolve('.'),
+        path.join(config.rootDir, config.distDir, config.publicDir),
+      ),
+    }),
   );
-  (express.static.mime as any).default_type = '';
   const port = parseInt(process.env.PORT || '8080', 10);
   startServer(app, port);
 }
 
-function startServer(app: Express, port: number) {
-  const server = app.listen(port, () => {
+async function startServer(app: Hono, port: number) {
+  const { serve } = await import('@hono/node-server');
+  const server = serve({ ...app, port }, () => {
     console.log(`ready: Listening on http://localhost:${port}/`);
   });
   server.on('error', (err: NodeJS.ErrnoException) => {
