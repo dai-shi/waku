b797613d385e6e49af39dcece8d78b5b7e542e8a
Alex Yang <himself65@outlook.com>
2023-11-27 13:48:04 -0600

    test: improvement

--- packages/waku/src/node-loader.ts
+++ packages/waku/src/node-loader.ts
@@ -1,3 +1,10 @@
+/**
+ * We manually call the hooks on react-server-dom-webpack/node-loader.
+ * 1. `react-server-dom-webpack/node-loader` uses Node.js 18 deprecated API.
+ * 2. `conditions` will not be read in ESM,
+ *      which will lead to a React warning on Node.js 20.
+ *      Refs: https://github.com/nodejs/node/issues/50885
+ */
 import type { LoadHook } from 'node:module';
 import { ResolveHook } from 'node:module';
 import * as RSDWNodeLoader from 'react-server-dom-webpack/node-loader';
