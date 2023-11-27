07f4462c29cb3a839027df2a690aa0f328a1da1a
Alex Yang <himself65@outlook.com>
2023-11-26 20:58:28 -0600

    fix: lint

--- packages/waku/src/node-loader.ts
+++ packages/waku/src/node-loader.ts
@@ -1,28 +1,36 @@
-import type { LoadHook } from "node:module";
-import { ResolveHook } from "node:module";
-import * as RSDWNodeLoader from "react-server-dom-webpack/node-loader";
-import { LoadFnOutput, LoadHookContext } from "module";
+import type { LoadHook } from 'node:module';
+import { ResolveHook } from 'node:module';
+import * as RSDWNodeLoader from 'react-server-dom-webpack/node-loader';
+import { LoadFnOutput, LoadHookContext } from 'module';
 
 export const load: LoadHook = async (url, context, nextLoad) => {
-  return RSDWNodeLoader.load(url, context, async (
-    reqUrl: string,
-    context?: LoadHookContext
-  ): Promise<LoadFnOutput> => {
-    const result: LoadFnOutput = await nextLoad(reqUrl, context);
-    if (result.format === "module") {
-      let { source } = result;
-      if (source && typeof source !== "string") {
-        source = source.toString();
-        return { ...result, source };
+  return RSDWNodeLoader.load(
+    url,
+    context,
+    async (
+      reqUrl: string,
+      context?: LoadHookContext,
+    ): Promise<LoadFnOutput> => {
+      const result: LoadFnOutput = await nextLoad(reqUrl, context);
+      if (result.format === 'module') {
+        let { source } = result;
+        if (source && typeof source !== 'string') {
+          source = source.toString();
+          return { ...result, source };
+        }
       }
-    }
-    return result
-  });
+      return result;
+    },
+  );
 };
 
 export const resolve: ResolveHook = async (specifier, context, nextResolve) => {
-  return RSDWNodeLoader.resolve(specifier, {
-    ...context,
-    conditions: [...context.conditions, "react-server"]
-  }, nextResolve);
+  return RSDWNodeLoader.resolve(
+    specifier,
+    {
+      ...context,
+      conditions: [...context.conditions, 'react-server'],
+    },
+    nextResolve,
+  );
 };
