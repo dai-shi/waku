41d37378c874338fd0156b859213230f93cf5a7d
Sophia Michelle Andren <20441876+sandren@users.noreply.github.com>
2023-11-27 20:13:29 -0800

    feat(website): mobile optimizations (#187)

--- packages/website/src/components/modal.tsx
+++ packages/website/src/components/modal.tsx
@@ -16,11 +16,11 @@ export const Modal = ({ isOpen, onClose, children }: ModalProps) => {
   if (!isOpen) return <Fragment />;
 
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center p-8">
+    <div className="fixed inset-0 z-50 flex items-center justify-center p-4 sm:p-8">
       <div className="inline-block overflow-clip rounded-2xl border-8 border-gray-950 bg-gray-900 p-2">
         <div
           ref={ref}
-          className="relative aspect-[1/1] w-full max-w-4xl overflow-y-auto p-6 text-left text-white sm:aspect-[4/3] lg:aspect-[16/9] lg:p-10"
+          className="relative aspect-[3/4] w-full max-w-sm overflow-y-auto p-3 text-left text-white sm:aspect-[4/3] sm:max-w-4xl sm:p-6 lg:aspect-[16/9] lg:p-10"
         >
           {children}
         </div>
