4b29c967d7675d315c6e4b1ab631ea8f36b37b51
Alex Yang <himself65@outlook.com>
2023-12-01 03:39:04 -0600

    lint: fix

--- e2e/ssr-basic.spec.ts
+++ e2e/ssr-basic.spec.ts
@@ -1,24 +1,24 @@
-import { expect } from "@playwright/test";
-import { execSync, exec, ChildProcess } from "node:child_process";
-import { fileURLToPath } from "node:url";
-import waitPort from "wait-port";
-import { test } from "./utils.js";
+import { expect } from '@playwright/test';
+import { execSync, exec, ChildProcess } from 'node:child_process';
+import { fileURLToPath } from 'node:url';
+import waitPort from 'wait-port';
+import { test } from './utils.js';
 
 const waku = fileURLToPath(
-  new URL("../packages/waku/dist/cli.js", import.meta.url)
+  new URL('../packages/waku/dist/cli.js', import.meta.url),
 );
 
 const commands = [
   {
-    command: "dev"
+    command: 'dev',
   },
   {
-    build: "build",
-    command: "start"
-  }
+    build: 'build',
+    command: 'start',
+  },
 ];
 
-const cwd = fileURLToPath(new URL("./fixtures/ssr-basic", import.meta.url));
+const cwd = fileURLToPath(new URL('./fixtures/ssr-basic', import.meta.url));
 
 for (const { build, command } of commands) {
   test.describe(`rsc-basic: ${command}`, () => {
@@ -27,24 +27,24 @@ for (const { build, command } of commands) {
     test.beforeAll(async () => {
       if (build) {
         execSync(`node ${waku} ${build}`, {
-          cwd
+          cwd,
         });
       }
       port = Math.floor(Math.random() * 10000) + 10000;
       console.log(`node ${waku} ${command}`);
-      console.log("cwd: ", cwd);
+      console.log('cwd: ', cwd);
       cp = exec(`node ${waku} ${command}`, {
         cwd,
         env: {
           ...process.env,
-          PORT: `${port}`
-        }
+          PORT: `${port}`,
+        },
       });
-      cp.on("message", (message) => {
-        console.log("cp message: ", message);
+      cp.on('message', (message) => {
+        console.log('cp message: ', message);
       });
       await waitPort({
-        port
+        port,
       });
     });
 
@@ -52,27 +52,27 @@ for (const { build, command } of commands) {
       cp.kill();
     });
 
-    test("increase counter", async ({ page }) => {
+    test('increase counter', async ({ page }) => {
       await page.goto(`http://localhost:${port}/`);
 
-      await expect(page.getByTestId("app-name")).toHaveText("Waku");
+      await expect(page.getByTestId('app-name')).toHaveText('Waku');
 
-      await expect(page.getByTestId("count")).toHaveText("0");
-      await page.getByTestId("increment").click();
-      await page.getByTestId("increment").click();
-      await page.getByTestId("increment").click();
-      await expect(page.getByTestId("count")).toHaveText("3");
+      await expect(page.getByTestId('count')).toHaveText('0');
+      await page.getByTestId('increment').click();
+      await page.getByTestId('increment').click();
+      await page.getByTestId('increment').click();
+      await expect(page.getByTestId('count')).toHaveText('3');
     });
 
-    test("no js environment should have first screen", async ({ browser }) => {
+    test('no js environment should have first screen', async ({ browser }) => {
       test.fixme();
       const noJSContext = await browser.newContext({
-        javaScriptEnabled: false
+        javaScriptEnabled: false,
       });
       const noJSPage = await noJSContext.newPage();
       await noJSPage.goto(`http://localhost:${port}/`);
-      await expect(noJSPage.getByTestId("app-name")).toHaveText("Waku");
-      await expect(noJSPage.getByTestId("count")).toHaveText("0");
+      await expect(noJSPage.getByTestId('app-name')).toHaveText('Waku');
+      await expect(noJSPage.getByTestId('count')).toHaveText('0');
     });
   });
 }
