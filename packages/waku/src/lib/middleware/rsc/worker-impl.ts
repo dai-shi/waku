ee382c4c1594825e4a67a49e204ff9107a3a1d7d
Alex Yang <himself65@outlook.com>
2023-12-01 02:46:49 -0600

    fix: code

--- packages/waku/src/lib/middleware/rsc/worker-impl.ts
+++ packages/waku/src/lib/middleware/rsc/worker-impl.ts
@@ -244,8 +244,8 @@ const transformRsfId = (prefixToRemove: string) =>
       for (let i = 0; i < lines.length; ++i) {
         const match = lines[i].match(
           new RegExp(
-            `^([0-9]+):{"id":"(?:file:///)?${prefixToRemove}(.*?)"(.*)$`,
-          ),
+            `^([0-9]+):{"id":"(?:file://?/)?${prefixToRemove}(.*?)"(.*)$`
+          )
         );
         if (match) {
           lines[i] = `${match[1]}:{"id":"${match[2]}"${match[3]}`;
