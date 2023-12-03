f96b0aa0bf035373b543402822ececc939802ad0
Alex Yang <himself65@outlook.com>
2023-12-02 22:13:17 -0600

    Update packages/waku/src/lib/middleware/rsc/utils.ts
    Co-authored-by: Daishi Kato <dai-shi@users.noreply.github.com>

--- packages/waku/src/lib/middleware/rsc/utils.ts
+++ packages/waku/src/lib/middleware/rsc/utils.ts
@@ -6,7 +6,7 @@ const posixSep = '/';
 
 // Refs: https://github.com/rollup/plugins/blob/d49bbe8dc5ec41157de5787c72c858f73be107ff/packages/pluginutils/src/normalizePath.ts
 export function normalizePath(filePath: string) {
-  return filePath.split(win32Sep).join(posixSep);
+  return filePath.split('\\').join('/');
 }
 
 export const encodeInput = (input: string) => {
