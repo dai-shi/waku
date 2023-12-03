3d808d9379a39c720ddd6b57b11c4235a5fa52ef
Alex Yang <himself65@outlook.com>
2023-12-02 22:13:32 -0600

    fix: remove unused

--- packages/waku/src/lib/middleware/rsc/utils.ts
+++ packages/waku/src/lib/middleware/rsc/utils.ts
@@ -1,9 +1,5 @@
 // This file should not include Node specific code.
 
-// copy from Node.js source code
-const win32Sep = '\\';
-const posixSep = '/';
-
 // Refs: https://github.com/rollup/plugins/blob/d49bbe8dc5ec41157de5787c72c858f73be107ff/packages/pluginutils/src/normalizePath.ts
 export function normalizePath(filePath: string) {
   return filePath.split('\\').join('/');
