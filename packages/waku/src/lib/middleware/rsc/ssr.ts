d552bd82f1a615c529aef83115ee480c31d4ec21
himself65 <himself65@outlook.com>
2023-12-02 22:50:11 -0600

    fix: code

--- packages/waku/src/lib/middleware/rsc/ssr.ts
+++ packages/waku/src/lib/middleware/rsc/ssr.ts
@@ -319,7 +319,9 @@ export const renderHtml = async <Context>(
               if (command === 'dev') {
                 const filePath = normalizePath(
                   file.startsWith('@fs/')
-                    ? file.slice('@fs/'.length)
+                    // FIXME This is ugly. We need to refactor it.
+                    // remove '@fs'(3) on Unix and '@fs/'(4) on Windows
+                    ? file.slice(2 + path.sep.length)
                     : path.join(config.rootDir, config.srcDir, file),
                 );
                 // FIXME This is ugly. We need to refactor it.
