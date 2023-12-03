248b67e5d8ead1e2410429bd5aafa5c4b0c9433a
Daishi Kato <dai-shi@users.noreply.github.com>
2023-12-03 14:11:01 +0900

    refactor: ssr for dev (#201)
    * removed unused deps
    
    * remove create transpiler hack
    
    * a small refactor
    
    * fix for router

--- packages/waku/src/lib/middleware/rsc/ssr.ts
+++ packages/waku/src/lib/middleware/rsc/ssr.ts
@@ -1,5 +1,4 @@
 import path from 'node:path';
-import fs from 'node:fs';
 import url from 'node:url';
 import { Server } from 'node:http';
 
@@ -19,11 +18,13 @@ const { renderToReadableStream } = RDServer;
 const { createFromReadableStream } = RSDWClient;
 
 // HACK for react-server-dom-webpack without webpack
-(globalThis as any).__waku_module_cache__ ||= new Map();
-(globalThis as any).__webpack_chunk_load__ ||= (id: string) =>
-  import(id).then((m) => (globalThis as any).__waku_module_cache__.set(id, m));
-(globalThis as any).__webpack_require__ ||= (id: string) =>
-  (globalThis as any).__waku_module_cache__.get(id);
+const moduleCache = new Map();
+(globalThis as any).__webpack_chunk_load__ ||= async (id: string) => {
+  const [filePath, command] = id.split('#');
+  const m = await loadServerFile(filePath!, (command as any) || 'start');
+  moduleCache.set(id, m);
+};
+(globalThis as any).__webpack_require__ ||= (id: string) => moduleCache.get(id);
 
 type Entries = {
   default: ReturnType<typeof defineEntries>;
@@ -65,8 +66,7 @@ export const shutdown = async () => {
   }
 };
 
-// This is exported only for createTranspiler
-export const loadServerFile = async (
+const loadServerFile = async (
   fname: string,
   command: 'dev' | 'build' | 'start',
 ) => {
@@ -75,25 +75,6 @@ export const loadServerFile = async (
   }
   const vite = await getViteServer();
   return vite.ssrLoadModule(fname);
-};
-
-// FIXME this is very hacky
-const createTranspiler = async (cleanupFns: Set<() => void>) => {
-  const { randomBytes } = await import('node:crypto');
-  return (filePath: string, name: string) => {
-    const temp = path.resolve(`.temp-${randomBytes(8).toString('hex')}.js`);
-    const code = `
-const { loadServerFile } = await import('${import.meta.url}');
-const { ${name} } = await loadServerFile('${url
-      .pathToFileURL(filePath)
-      .toString()
-      .slice('file://'.length)}', 'dev');
-export { ${name} }
-`;
-    fs.writeFileSync(temp, code);
-    cleanupFns.add(() => fs.unlinkSync(temp));
-    return temp;
-  };
 };
 
 const getEntriesFile = (
@@ -294,9 +275,6 @@ export const renderHtml = async <Context>(
     throw e;
   }
   const { splitHTML } = config.ssr;
-  const cleanupFns = new Set<() => void>();
-  const transpile =
-    command === 'dev' ? await createTranspiler(cleanupFns) : undefined;
   const moduleMap = new Proxy(
     {} as Record<
       string,
@@ -340,9 +318,11 @@ export const renderHtml = async <Context>(
                     filePath.slice(wakuDist.length).replace(/\.\w+$/, '');
                   return { id, chunks: [id], name };
                 }
-                const id = url
-                  .pathToFileURL(transpile!(filePath, name))
-                  .toString();
+                const id =
+                  url
+                    .pathToFileURL(filePath)
+                    .toString()
+                    .slice('file://'.length) + '#dev';
                 return { id, chunks: [id], name };
               }
               // command !== 'dev'
@@ -384,7 +364,5 @@ export const renderHtml = async <Context>(
   )
     .pipeThrough(rectifyHtml())
     .pipeThrough(interleave(...splitHTML(htmlStr)));
-  cleanupFns.forEach((fn) => fn());
-  cleanupFns.clear();
   return [readable, nextCtx];
 };
