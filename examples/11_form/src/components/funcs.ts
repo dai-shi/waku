a46a7dcce55802561ae21283a14cac7cf1db5ff6
daishi <daishi@axlight.com>
2023-12-04 12:55:36 +0900

    breaking: no async local storage

--- examples/11_form/src/components/funcs.ts
+++ examples/11_form/src/components/funcs.ts
@@ -1,15 +1,15 @@
 'use server';
 
-import { rerender } from 'waku/server';
+import type { RenderContext } from 'waku/server';
 
 // module state on server
 let message = '';
 
 export const getMessage = () => message;
 
-export const greet = (formData: FormData) => {
+export function greet(this: RenderContext, formData: FormData) {
   message = `Hello ${formData.get('name') || 'Anonymous'} from server!`;
-  rerender('');
-};
+  this.rerender('');
+}
 
 export const increment = (count: number) => count + 1;
