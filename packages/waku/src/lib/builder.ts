9d9945b68931293e867014ab576a3198f29d1a41
daishi <daishi@axlight.com>
2023-12-05 09:48:13 +0900

    wip: debugging

--- packages/waku/src/lib/builder.ts
+++ packages/waku/src/lib/builder.ts
@@ -366,6 +366,12 @@ const emitVercelOutput = async (
   const dstDir = joinPath(config.rootDir, config.distDir, '.vercel', 'output');
   for (const file of [...clientFiles, ...rscFiles, ...htmlFiles]) {
     const dstFile = joinPath(dstDir, 'static', relativePath(srcDir, file));
+    console.log({ file, dstFile });
+    console.log('****** joinPath', joinPath(dstFile, '..'));
+    console.log(
+      '****** relativePath',
+      relativePath(joinPath(dstFile, '..'), file),
+    );
     if (!existsSync(dstFile)) {
       await mkdir(joinPath(dstFile, '..'), { recursive: true });
       await symlink(relativePath(joinPath(dstFile, '..'), file), dstFile);
