5a35461c6c746a7de2f00842e2355cc7b4490e8a
Daishi Kato <dai-shi@users.noreply.github.com>
2023-12-05 10:39:57 +0900

    Apply suggestions from code review

--- packages/waku/src/node-loader.ts
+++ packages/waku/src/node-loader.ts
@@ -5,9 +5,8 @@
  *      which will lead to a React warning on Node.js 20.
  *      Refs: https://github.com/nodejs/node/issues/50885
  */
-import type { LoadHook, ResolveHook } from 'node:module';
+import type { LoadHook, ResolveHook, LoadFnOutput, LoadHookContext } from 'node:module';
 import * as RSDWNodeLoader from 'react-server-dom-webpack/node-loader';
-import type { LoadFnOutput, LoadHookContext } from 'module';
 
 export const load: LoadHook = async (url, context, nextLoad) => {
   return RSDWNodeLoader.load(
