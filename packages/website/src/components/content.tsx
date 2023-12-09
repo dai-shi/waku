11e5326a2ce68b3e384bf8dcb6c9bb5b438f141c
daishi <daishi@axlight.com>
2023-12-09 15:56:42 +0900

    copy readme before deploy

--- packages/website/src/components/content.tsx
+++ packages/website/src/components/content.tsx
@@ -1,11 +1,12 @@
-import { readFileSync } from 'node:fs';
+import { existsSync, readFileSync } from 'node:fs';
 // @ts-expect-error no exported member
 import { MDXRemote } from 'next-mdx-remote/rsc';
 
 import { Code } from './code.js';
 
 export const Content = () => {
-  const file = readFileSync('../../README.md', 'utf8');
+  const fname = existsSync('./README.md') ? './README.md' : '../../README.md';
+  const file = readFileSync(fname, 'utf8');
   const source = `## Introduction${file
     .split('## Introduction')[1]
     ?.split('## Tweets')[0]}`;
