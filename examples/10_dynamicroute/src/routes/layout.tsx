ebde88c38578db7e46ee360f5f1e8bd0f89ee65f
Daishi Kato <dai-shi@users.noreply.github.com>
2023-12-09 19:48:40 +0900

    chore: update router examples (#234)
    * move 07 to 10
    
    * modify 07
    
    * fix tsconfig

--- examples/10_dynamicroute/src/routes/layout.tsx
+++ examples/10_dynamicroute/src/routes/layout.tsx
@@ -2,20 +2,66 @@ import type { ReactNode } from 'react';
 
 import { Link } from 'waku/router/client';
 
+const Pending = ({ isPending }: { isPending: boolean }) => (
+  <span
+    style={{
+      marginLeft: 5,
+      transition: 'opacity 75ms 100ms',
+      opacity: isPending ? 1 : 0,
+    }}
+  >
+    Pending...
+  </span>
+);
+
 const HomeLayout = ({ children }: { children: ReactNode }) => (
   <div>
     <ul>
       <li>
-        <Link href="/">Home</Link>
+        <Link
+          href="/"
+          pending={<Pending isPending />}
+          notPending={<Pending isPending={false} />}
+        >
+          Home
+        </Link>
       </li>
       <li>
-        <Link href="/foo">Foo</Link>
+        <Link
+          href="/foo"
+          pending={<Pending isPending />}
+          notPending={<Pending isPending={false} />}
+        >
+          Foo
+        </Link>
       </li>
       <li>
-        <Link href="/bar">Bar</Link>
+        <Link
+          href="/bar"
+          unstable_prefetchOnEnter
+          pending={<Pending isPending />}
+          notPending={<Pending isPending={false} />}
+        >
+          Bar
+        </Link>
       </li>
       <li>
-        <Link href="/baz">Baz</Link>
+        <Link
+          href="/nested/baz"
+          pending={<Pending isPending />}
+          notPending={<Pending isPending={false} />}
+        >
+          Nested / Baz
+        </Link>
+      </li>
+      <li>
+        <Link
+          href="/nested/qux"
+          pending={<Pending isPending />}
+          notPending={<Pending isPending={false} />}
+        >
+          Nested / Baz
+        </Link>
       </li>
     </ul>
     {children}
