727700e909762822d08f56ba67ff87a102291fbb
daishi <daishi@axlight.com>
2023-12-11 17:41:00 +0900

    did i try this?

--- packages/website/vite.config.ts
+++ packages/website/vite.config.ts
@@ -1,15 +1,6 @@
-import url from 'node:url';
-import path from 'node:path';
-
 import { defineConfig, loadEnv } from 'vite';
 
 export default ({ mode }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
-
-  return defineConfig({
-    root: path.dirname(url.fileURLToPath(import.meta.url)),
-    ssr: {
-      external: ['next-mdx-remote'],
-    },
-  });
+  return defineConfig({});
 };
