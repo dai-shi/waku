9cfa82c597a9a484586f91896addadfdfc154720
Sophia Michelle Andren <20441876+sandren@users.noreply.github.com>
2023-12-11 07:28:47 +0000

    feat(website): add blog

---
+++ packages/website/src/components/article.tsx
@@ -0,0 +1,18 @@
+import { readFileSync } from 'node:fs';
+// @ts-expect-error no exported member
+import { MDXRemote } from 'next-mdx-remote/rsc';
+
+import { components } from './mdx.js';
+
+export const Article = () => {
+  const fname = './src/contents/post-001.mdx';
+  const source = readFileSync(fname, 'utf8');
+
+  return (
+    <MDXRemote
+      source={source}
+      components={components}
+      options={{ parseFrontmatter: true }}
+    />
+  );
+};
