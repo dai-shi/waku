137fac79cc873314ee593b21976f51b8d01e449c
daishi <daishi@axlight.com>
2023-12-11 17:48:11 +0900

    copy contents too

--- packages/website/src/components/article.tsx
+++ packages/website/src/components/article.tsx
@@ -1,11 +1,13 @@
-import { readFileSync } from 'node:fs';
+import { existsSync, readFileSync } from 'node:fs';
 // @ts-expect-error no exported member
 import { MDXRemote } from 'next-mdx-remote/rsc';
 
 import { components } from './mdx.js';
 
 export const Article = () => {
-  const fname = './src/contents/post-001.mdx';
+  const fname = existsSync('./contents')
+    ? './contents/post-001.mdx'
+    : './src/contents/post-001.mdx';
   const source = readFileSync(fname, 'utf8');
 
   return (
