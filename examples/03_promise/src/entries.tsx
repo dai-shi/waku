c6b6d9c388f921b8b65433538261ad0da987b1a9
daishi <daishi@axlight.com>
2023-12-15 19:29:35 +0900

    revert example 03 changes

--- examples/03_promise/src/entries.tsx
+++ examples/03_promise/src/entries.tsx
@@ -1,5 +1,6 @@
 import { lazy } from 'react';
 import { defineEntries } from 'waku/server';
+import { Children } from 'waku/client';
 
 const App = lazy(() => import('./components/App.js'));
 
@@ -7,7 +8,11 @@ export default defineEntries(
   // renderEntries
   async (input) => {
     return {
-      App: <App name={input || 'Waku'} />,
+      App: (
+        <App name={input || 'Waku'}>
+          <Children />
+        </App>
+      ),
     };
   },
   // getBuildConfig
@@ -26,7 +31,11 @@ export default defineEntries(
         return {
           input: '',
           unstable_render: ({ createElement, Slot }) =>
-            createElement(Slot, { id: 'App' }),
+            createElement(
+              Slot,
+              { id: 'App' },
+              createElement('h3', null, 'A client element'),
+            ),
         };
       default:
         return null;
