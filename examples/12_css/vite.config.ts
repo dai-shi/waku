c24db4d2d7f1fcb5ff24a015203ddcf6264fc358
Alex Yang <himself65@outlook.com>
2023-12-17 20:29:16 -0600

    fix: config

--- examples/12_css/vite.config.ts
+++ examples/12_css/vite.config.ts
@@ -1,21 +1,13 @@
 import { defineConfig } from 'vite';
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 import styleX from 'vite-plugin-stylex';
-import commonjs from 'vite-plugin-commonjs';
 
 export default defineConfig({
+  ssr: {
+    external: ['@stylexjs/stylex'],
+  },
   plugins: [
     vanillaExtractPlugin({ emitCssInSsr: true }),
-    commonjs({
-      filter(id) {
-        if (
-          id.includes('node_modules/@stylexjs') ||
-          id.includes('node_modules/styleq')
-        ) {
-          return true;
-        }
-      },
-    }),
     styleX(),
   ],
 });
