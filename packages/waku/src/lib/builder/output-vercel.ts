f88964ed83959f0f97952a1940e5f2659233473b
Sophia Michelle Andren <20441876+sandren@users.noreply.github.com>
2023-12-23 23:41:13 +0000

    fix(core): temporarily disable routes entry

--- packages/waku/src/lib/builder/output-vercel.ts
+++ packages/waku/src/lib/builder/output-vercel.ts
@@ -19,73 +19,71 @@ export const emitVercelOutput = async (
     { recursive: true },
   );
 
-  // @TODO restore serverless function
-
   // for serverless function
-  // const serverlessDir = path.join(
-  //   outputDir,
-  //   'functions',
-  //   config.rscPath + '.func',
-  // );
-  // mkdirSync(path.join(serverlessDir, config.distDir), {
-  //   recursive: true,
-  // });
-  // mkdirSync(path.join(serverlessDir, 'node_modules'), {
-  //   recursive: true,
-  // });
-  // cpSync(
-  //   path.join(rootDir, 'node_modules', 'waku'),
-  //   path.join(serverlessDir, 'node_modules', 'waku'),
-  //   { dereference: true, recursive: true },
-  // );
-  // cpSync(
-  //   path.join(rootDir, config.distDir),
-  //   path.join(serverlessDir, config.distDir),
-  //   { recursive: true },
-  // );
-  // const vcConfigJson = {
-  //   runtime: 'nodejs18.x',
-  //   handler: 'serve.js',
-  //   launcherType: 'Nodejs',
-  // };
-  // writeFileSync(
-  //   path.join(serverlessDir, '.vc-config.json'),
-  //   JSON.stringify(vcConfigJson, null, 2),
-  // );
-  // writeFileSync(
-  //   path.join(serverlessDir, 'package.json'),
-  //   JSON.stringify({ type: 'module' }, null, 2),
-  // );
-  //   writeFileSync(
-  //     path.join(serverlessDir, 'serve.js'),
-  //     `
-  // import path from 'node:path';
-  // import fs from 'node:fs';
-  // import { connectMiddleware } from 'waku';
-  // const entries = import(path.resolve('${config.distDir}', '${config.entriesJs}'));
-  // export default function handler(req, res) {
-  //   connectMiddleware({ entries, ssr: ${ssr} })(req, res, () => {
-  //     const fname = path.join(
-  //       '${config.distDir}',
-  //       '${config.publicDir}',
-  //       req.url,
-  //       path.extname(req.url) ? '' : '${config.indexHtml}',
-  //     );
-  //     if (fs.existsSync(fname)) {
-  //       if (fname.endsWith('.html')) {
-  //         res.setHeader('content-type', 'text/html; charset=utf-8');
-  //       } else if (fname.endsWith('.txt')) {
-  //         res.setHeader('content-type', 'text/plain');
-  //       }
-  //       fs.createReadStream(fname).pipe(res);
-  //       return;
-  //     }
-  //     res.statusCode = 404;
-  //     res.end();
-  //   });
-  // }
-  // `,
-  //   );
+  const serverlessDir = path.join(
+    outputDir,
+    'functions',
+    config.rscPath + '.func',
+  );
+  mkdirSync(path.join(serverlessDir, config.distDir), {
+    recursive: true,
+  });
+  mkdirSync(path.join(serverlessDir, 'node_modules'), {
+    recursive: true,
+  });
+  cpSync(
+    path.join(rootDir, 'node_modules', 'waku'),
+    path.join(serverlessDir, 'node_modules', 'waku'),
+    { dereference: true, recursive: true },
+  );
+  cpSync(
+    path.join(rootDir, config.distDir),
+    path.join(serverlessDir, config.distDir),
+    { recursive: true },
+  );
+  const vcConfigJson = {
+    runtime: 'nodejs18.x',
+    handler: 'serve.js',
+    launcherType: 'Nodejs',
+  };
+  writeFileSync(
+    path.join(serverlessDir, '.vc-config.json'),
+    JSON.stringify(vcConfigJson, null, 2),
+  );
+  writeFileSync(
+    path.join(serverlessDir, 'package.json'),
+    JSON.stringify({ type: 'module' }, null, 2),
+  );
+  writeFileSync(
+    path.join(serverlessDir, 'serve.js'),
+    `
+  import path from 'node:path';
+  import fs from 'node:fs';
+  import { connectMiddleware } from 'waku';
+  const entries = import(path.resolve('${config.distDir}', '${config.entriesJs}'));
+  export default function handler(req, res) {
+    connectMiddleware({ entries, ssr: ${ssr} })(req, res, () => {
+      const fname = path.join(
+        '${config.distDir}',
+        '${config.publicDir}',
+        req.url,
+        path.extname(req.url) ? '' : '${config.indexHtml}',
+      );
+      if (fs.existsSync(fname)) {
+        if (fname.endsWith('.html')) {
+          res.setHeader('content-type', 'text/html; charset=utf-8');
+        } else if (fname.endsWith('.txt')) {
+          res.setHeader('content-type', 'text/plain');
+        }
+        fs.createReadStream(fname).pipe(res);
+        return;
+      }
+      res.statusCode = 404;
+      res.end();
+    });
+  }
+  `,
+  );
 
   const overrides = Object.fromEntries(
     rscFiles
@@ -95,20 +93,24 @@ export const emitVercelOutput = async (
         { contentType: 'text/plain' },
       ]),
   );
-  const basePrefix = config.basePath + config.rscPath + '/';
-  const routes = [
-    { src: basePrefix + '(.*)', dest: basePrefix },
-    ...(ssr
-      ? htmlFiles.map((htmlFile) => {
-          const file = config.basePath + path.relative(publicDir, htmlFile);
-          const src = file.endsWith('/' + config.indexHtml)
-            ? file.slice(0, -('/' + config.indexHtml).length) || '/'
-            : file;
-          return { src, dest: basePrefix };
-        })
-      : []),
-  ];
-  const configJson = { version: 3, overrides, routes };
+  // @TODO restore routes
+
+  // const basePrefix = config.basePath + config.rscPath + '/';
+  // const routes = [
+  //   { src: basePrefix + '(.*)', dest: basePrefix },
+  //   ...(ssr
+  //     ? htmlFiles.map((htmlFile) => {
+  //         const file = config.basePath + path.relative(publicDir, htmlFile);
+  //         const src = file.endsWith('/' + config.indexHtml)
+  //           ? file.slice(0, -('/' + config.indexHtml).length) || '/'
+  //           : file;
+  //         return { src, dest: basePrefix };
+  //       })
+  //     : []),
+  // ];
+
+  // @TODO restore routes
+  const configJson = { version: 3, overrides };
   mkdirSync(outputDir, { recursive: true });
   writeFileSync(
     path.join(outputDir, 'config.json'),
