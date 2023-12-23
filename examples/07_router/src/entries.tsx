f32fe5590bc748a72de9f42a71ebc442a6477203
daishi <daishi@axlight.com>
2023-12-23 21:57:41 +0900

    avoid lazy

--- examples/07_router/src/entries.tsx
+++ examples/07_router/src/entries.tsx
@@ -1,12 +1,19 @@
-import { lazy } from 'react';
+// import { lazy } from 'react';
 import { createPages } from 'waku/router/server';
 
-const HomeLayout = lazy(() => import('./components/HomeLayout.js'));
-const HomePage = lazy(() => import('./components/HomePage.js'));
-const FooPage = lazy(() => import('./components/FooPage.js'));
-const BarPage = lazy(() => import('./components/BarPage.js'));
-const NestedBazPage = lazy(() => import('./components/NestedBazPage.js'));
-const NestedQuxPage = lazy(() => import('./components/NestedQuxPage.js'));
+// FIXME
+// const HomeLayout = lazy(() => import('./components/HomeLayout.js'));
+// const HomePage = lazy(() => import('./components/HomePage.js'));
+// const FooPage = lazy(() => import('./components/FooPage.js'));
+// const BarPage = lazy(() => import('./components/BarPage.js'));
+// const NestedBazPage = lazy(() => import('./components/NestedBazPage.js'));
+// const NestedQuxPage = lazy(() => import('./components/NestedQuxPage.js'));
+import HomeLayout from './components/HomeLayout.js';
+import HomePage from './components/HomePage.js';
+import FooPage from './components/FooPage.js';
+import BarPage from './components/BarPage.js';
+import NestedBazPage from './components/NestedBazPage.js';
+import NestedQuxPage from './components/NestedQuxPage.js';
 
 export default createPages(async ({ createPage }) => {
   createPage({
