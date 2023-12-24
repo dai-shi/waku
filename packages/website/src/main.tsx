4bdf6d871d8b80fdb6bff59df20be75ebac4609d
Sophia Michelle Andren <20441876+sandren@users.noreply.github.com>
2023-12-24 00:18:17 +0000

    fix(website): restore hydration

--- packages/website/src/main.tsx
+++ packages/website/src/main.tsx
@@ -1,5 +1,5 @@
 import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import { createRoot, hydrateRoot } from 'react-dom/client';
 import { Router } from 'waku/router/client';
 
 const rootElement = (
@@ -11,7 +11,11 @@ const rootElement = (
 // FIXME temporary fix, doesn't feel ideal.
 function init() {
   try {
-    createRoot(document.body).render(rootElement);
+    if ((globalThis as any).__WAKU_SSR_ENABLED__) {
+      hydrateRoot(document.body, rootElement);
+    } else {
+      createRoot(document.body).render(rootElement);
+    }
   } catch (e) {
     console.log('retrying as an error is caught: ' + e);
     setTimeout(init);
