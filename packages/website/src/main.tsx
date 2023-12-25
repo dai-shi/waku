ac2f3e6c32fd9ce12a356ef3991e493fe0e9c63a
daishi <daishi@axlight.com>
2023-12-25 09:51:59 +0900

    website add error boundary

--- packages/website/src/main.tsx
+++ packages/website/src/main.tsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react';
 import { createRoot, hydrateRoot } from 'react-dom/client';
 import { Router } from 'waku/router/client';
 
+import { ErrorBoundary } from './components/ErrorBoundary.js';
+
 const rootElement = (
   <StrictMode>
-    <Router />
+    <ErrorBoundary fallback={(error) => <h1>{String(error)}</h1>}>
+      <Router />
+    </ErrorBoundary>
   </StrictMode>
 );
 
