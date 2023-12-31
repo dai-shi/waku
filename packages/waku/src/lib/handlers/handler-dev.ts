f34d992503b147b4f7a095b03b91e4159b7f33aa
daishi <daishi@axlight.com>
2023-12-31 19:08:01 +0900

    Revert "does this pass?"
    This reverts commit 31a83a7df41706c83ee485bf24ea4551039fc7e3.

--- packages/waku/src/lib/handlers/handler-dev.ts
+++ packages/waku/src/lib/handlers/handler-dev.ts
@@ -48,9 +48,7 @@ export function createHandler<
     const mergedViteConfig = await mergeUserViteConfig({
       base: config.basePath,
       optimizeDeps: {
-        // FIXME adding `react-dom` makes more sense.
-        include: ['react-server-dom-webpack/client'],
-        // include: ['react-server-dom-webpack/client', 'react', 'react-dom'],
+        include: ['react-server-dom-webpack/client', 'react', 'react-dom'],
         exclude: ['waku'],
       },
       plugins: [
