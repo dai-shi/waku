204adec8bc47a3082ce6d69547a565a35d1cd2aa
Alex Yang <himself65@outlook.com>
2024-01-01 04:17:03 -0600

    fix: logging

--- e2e/utils.ts
+++ e2e/utils.ts
@@ -18,7 +18,6 @@ export async function getFreePort(): Promise<number> {
 
 export function collectChildProcess(cp: ChildProcess) {
   childProcessSet.add(cp);
-  // disable debug logs in CI, as they are too verbose and mostly useless
   if (!process.env.CI) {
     cp.stdout?.on('data', (data) => {
       console.log(`${data}`);
@@ -31,7 +30,7 @@ export function collectChildProcess(cp: ChildProcess) {
 }
 
 export const test = basicTest.extend({
-  page: async ({ page }, use) => {
+  page: async ({ page }, use, testInfo) => {
     const unexpectedErrors: RegExp[] = [
       /^You did not run Node.js with the `--conditions react-server` flag./,
       /^\(node:14372\)/,
@@ -46,30 +45,25 @@ export const test = basicTest.extend({
     page.on('console', callback);
     await use(page);
     page.off('console', callback);
-    console.log('Page console messages:');
-    console.log(messages.join('\n'));
-    console.log('-'.repeat(80));
+    if (messages.length > 0) {
+      console.log(`${testInfo.title} page console messages:`);
+      console.log(messages.join('\n'));
+      console.log('-'.repeat(80));
+    }
   },
 });
 
-test.afterEach(
-  async (
-    // eslint-disable-next-line no-empty-pattern
-    {},
-    testInfo,
-  ) => {
-    if (testInfo.status !== testInfo.expectedStatus) {
-      console.error(`Test failed: ${testInfo.title}`);
-      childProcessSet.forEach((cp) => {
-        if (childProcessLoggedSet.has(cp)) {
-          return;
-        }
-        console.error(`Child process(${cp.pid}) stdout:`);
-        cp.stdout?.pipe(process.stdout);
-        console.error(`Child process(${cp.pid}) stderr:`);
-        cp.stderr?.pipe(process.stderr);
-        childProcessLoggedSet.add(cp);
-      });
-    }
-  },
-);
+test.afterEach(async () => {
+  if (process.env.CI) {
+    childProcessSet.forEach((cp) => {
+      if (childProcessLoggedSet.has(cp)) {
+        return;
+      }
+      console.error(`Child process(${cp.pid}) stdout:`);
+      cp.stdout?.pipe(process.stdout);
+      console.error(`Child process(${cp.pid}) stderr:`);
+      cp.stderr?.pipe(process.stderr);
+      childProcessLoggedSet.add(cp);
+    });
+  }
+});
