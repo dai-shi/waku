08d188b8457dd215a1480ff07fcfd67466701578
Sophia Michelle Andren <20441876+sandren@users.noreply.github.com>
2024-01-07 20:43:03 +0000

    feat(website): UI/UX improvements

--- packages/website/src/templates/root-layout.tsx
+++ packages/website/src/templates/root-layout.tsx
@@ -76,7 +76,7 @@ const Background = () => {
           className="h-full w-full object-cover object-center"
         />
       </div>
-      <div className="absolute inset-0 z-10 h-full w-full bg-gradient-radial from-transparent to-gray-900/50" />
+      <div className="absolute inset-0 z-10 h-full w-full bg-gradient-radial from-transparent to-black/25" />
     </div>
   );
 };
