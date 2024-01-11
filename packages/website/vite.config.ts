f290ae982bf035a048d32aa824bcb536f29a90fb
daishi <daishi@axlight.com>
2024-01-11 12:10:59 +0900

    fix optimizeDeps

--- packages/website/vite.config.ts
+++ packages/website/vite.config.ts
@@ -4,11 +4,8 @@ export default defineConfig(({ mode }) => {
   if (mode === 'development') {
     return {
       optimizeDeps: {
-        // FIXME this causes an error with `--with-ssr`
-        // include: ['jotai', 'clsx'],
-        include: ['jotai', 'clsx'],
+        include: ['react', 'jotai', 'clsx'],
       },
-
       ssr: {
         external: ['next-mdx-remote'],
       },
