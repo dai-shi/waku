9e08436ca8e232fc8f2ef28995ba7c183f537ab4
daishi <daishi@axlight.com>
2024-01-15 13:06:40 +0900

    fix(examples): 12_css to prefer dev-only external workaround to commonjs plugin

--- examples/12_css/vite.config.ts
+++ examples/12_css/vite.config.ts
@@ -1,24 +1,12 @@
 import { defineConfig } from 'vite';
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
-import commonjs from 'vite-plugin-commonjs';
 import { stylexPlugin } from 'vite-plugin-stylex-dev';
 
-export default defineConfig({
-  ssr: {
-    external: ['@stylexjs/stylex'],
-  },
-  plugins: [
-    vanillaExtractPlugin({ emitCssInSsr: true }),
-    // @ts-expect-error not callable FIXME why not callable?
-    commonjs({
-      filter(id: string) {
-        // `node_modules` is exclude by default, so we need to include it explicitly
-        // https://github.com/vite-plugin/vite-plugin-commonjs/blob/v0.10.1/src/index.ts#L141-L142
-        if (id.includes('node_modules/classnames')) {
-          return true;
-        }
-      },
-    }),
-    stylexPlugin(),
-  ],
-});
+export default defineConfig(({ mode }) => ({
+  ...(mode === 'development' && {
+    ssr: {
+      external: ['@stylexjs/stylex', 'classnames'],
+    },
+  }),
+  plugins: [vanillaExtractPlugin({ emitCssInSsr: true }), stylexPlugin()],
+}));
