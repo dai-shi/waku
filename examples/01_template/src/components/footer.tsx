3deb37c8107a72623ef74c6d170c70eb545e48cd
Sophia Michelle Andren <20441876+sandren@users.noreply.github.com>
2024-01-16 03:22:58 +0000

    feat(starter): add new waku starter

---
+++ examples/01_template/src/components/footer.tsx
@@ -0,0 +1,13 @@
+export const Footer = () => {
+  return (
+    <footer className="fixed bottom-0 left-0 p-6">
+      <div>
+        visit{' '}
+        <a href="https://waku.gg/" target="_blank" rel="noreferrer">
+          waku.gg
+        </a>{' '}
+        to learn more
+      </div>
+    </footer>
+  );
+};
---
+++ examples/01_template/tailwind.config.js
@@ -0,0 +1,5 @@
+/** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{ts,tsx}'],
+};
