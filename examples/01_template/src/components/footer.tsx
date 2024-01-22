7de378dfddd3f71f328058037f12eaddc1fa481a
Sophia Michelle Andren <20441876+sandren@users.noreply.github.com>
2024-01-21 19:02:26 -0800

    feat(starter): add gitignore and remove @apply directives (#409)
    * feat(starter): add gitignore
    
    * fix(starter): restore title for e2e
    
    * run prettier
    
    ---------
    
    Co-authored-by: daishi <daishi@axlight.com>

--- examples/01_template/src/components/footer.tsx
+++ examples/01_template/src/components/footer.tsx
@@ -3,7 +3,12 @@ export const Footer = () => {
     <footer className="fixed bottom-0 left-0 p-6">
       <div>
         visit{' '}
-        <a href="https://waku.gg/" target="_blank" rel="noreferrer">
+        <a
+          href="https://waku.gg/"
+          target="_blank"
+          rel="noreferrer"
+          className="mt-4 inline-block underline"
+        >
           waku.gg
         </a>{' '}
         to learn more
