7c5ce23a1a60ba43b0627e38e1cc98e0256e9b16
daishi <daishi@axlight.com>
2024-01-23 21:39:35 +0900

    wip: serve netlify

---
+++ packages/waku/src/lib/builder/serve-netlify.ts
@@ -0,0 +1,20 @@
+import { Hono } from 'hono';
+import type { Config, Context } from '@netlify/functions';
+
+import { honoMiddleware } from '../middleware/hono-prd.js';
+
+const ssr = !!import.meta.env.WAKU_BUILD_SSR;
+const loadEntries = () => import(import.meta.env.WAKU_ENTRIES_FILE!);
+const env: Record<string, string> = process.env as any;
+
+const app = new Hono();
+app.use('*', honoMiddleware({ loadEntries, ssr, env }));
+export default async (req: Request, context: Context) =>
+  app.fetch(req, { context });
+
+export const config: Config = {
+  path: [
+    `/${import.meta.env.WAKU_CONFIG_RSC_PATH}/*`,
+    // TODO add html paths
+  ],
+};
