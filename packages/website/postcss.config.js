d9dc12e127b52eab92faad3eac69099a5b537305
Mohammad Bagher Abiyat <zorofight94@gmail.com>
2024-01-25 17:00:50 +0330

    fix(website): prop mismatch error with ssr by turning off autoprefixing (#426)
    * turn off prefixer for now
    
    * format
    
    ---------
    
    Co-authored-by: Daishi Kato <dai-shi@users.noreply.github.com>

--- packages/website/postcss.config.js
+++ packages/website/postcss.config.js
@@ -3,6 +3,9 @@ export default {
   plugins: {
     'tailwindcss/nesting': {},
     tailwindcss: {},
-    autoprefixer: {},
+    autoprefixer: {
+      // TODO: This should be removed after `bright` https://github.com/postcss/autoprefixer?tab=readme-ov-file#control-comments for auto prefixes
+      add: false,
+    },
   },
 };
