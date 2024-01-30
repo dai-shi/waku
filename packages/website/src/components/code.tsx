95ba151c17bbeb7060c5b8cfcfeab672f376839d
Sophia Michelle Andren <20441876+sandren@users.noreply.github.com>
2024-01-30 05:02:40 +0000

    fix(website): cache global shiki instance

--- packages/website/src/components/code.tsx
+++ packages/website/src/components/code.tsx
@@ -1,17 +1,10 @@
-import { getHighlighter } from 'shiki';
-
-import theme from '../theme.json';
+import { highlighter } from '../lib/index.js';
 
 type CodeProps = {
   code: string;
 };
 
 export const Code = async ({ code, ...rest }: CodeProps) => {
-  const highlighter = await getHighlighter({
-    langs: ['tsx'],
-    themes: [theme as any],
-  });
-
   const html = highlighter.codeToHtml(code.trim(), {
     lang: 'tsx',
     theme: 'lucy',
