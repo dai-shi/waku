48e691d424c46610f3e9edadc066e5579323cf3c
daishi <daishi@axlight.com>
2024-01-31 10:09:26 +0900

    fix ssr option

--- packages/waku/src/lib/builder/serve-vercel.ts
+++ packages/waku/src/lib/builder/serve-vercel.ts
@@ -4,11 +4,12 @@ import { getRequestListener } from '@hono/node-server';
 
 import { honoMiddleware } from '../middleware/hono-prd.js';
 
+const ssr = !!import.meta.env.WAKU_BUILD_SSR;
 const loadEntries = () => import(import.meta.env.WAKU_ENTRIES_FILE!);
 const env: Record<string, string> = process.env as any;
 
 const app = new Hono();
-app.use('*', honoMiddleware({ loadEntries, env }));
+app.use('*', honoMiddleware({ loadEntries, ssr, env }));
 const requestListener = getRequestListener(app.fetch);
 
 export default function handler(req: IncomingMessage, res: ServerResponse) {
