02468d90c0de901027e862da8b0b3945157803d7
daishi <daishi@axlight.com>
2024-02-02 15:47:59 +0900

    fix lint

--- examples/12_css/vite.config.ts
+++ examples/12_css/vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 import { stylexPlugin } from 'vite-plugin-stylex-dev';
 
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   plugins: [vanillaExtractPlugin({ emitCssInSsr: true }), stylexPlugin()],
-}));
+});
