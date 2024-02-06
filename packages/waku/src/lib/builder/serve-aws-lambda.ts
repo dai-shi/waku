92604018948c11d2672f4a18d4731503babfb688
Andreas Heissenberger <andreas@heissenberger.at>
2024-02-06 15:28:19 +0100

    fix: AWS HTTP API V2 breaks RSC POST requests url needed for server actions

--- packages/waku/src/lib/builder/serve-aws-lambda.ts
+++ packages/waku/src/lib/builder/serve-aws-lambda.ts
@@ -1,8 +1,7 @@
 import { Hono } from 'hono';
-import { handle } from 'hono/aws-lambda';
-import { serveStatic } from '@hono/node-server/serve-static';
-
 import { honoMiddleware } from '../middleware/hono-prd.js';
+import { handle, type LambdaContext, type LambdaEvent } from 'hono/aws-lambda';
+import { serveStatic } from '@hono/node-server/serve-static';
 
 const ssr = !!import.meta.env.WAKU_BUILD_SSR;
 const distDir = import.meta.env.WAKU_CONFIG_DIST_DIR;
@@ -15,4 +14,32 @@ const app = new Hono();
 app.use('*', serveStatic({ root: `${distDir}/${publicDir}` }));
 app.use('*', honoMiddleware({ loadEntries, ssr, env }));
 
-export const handler = handle(app);
+/*
+ FIX: AWS HTTP API V2 breaks RSC POST requests needed for server actions
+    - AWS HTTP API V2 sets rawPath to a decoded path
+    - We need to encode the string after /RSC/ and replace the orginal rawPath
+    - Hono bug: https://github.com/honojs/hono/issues/2156
+*/
+
+const fixHTTPAPIV2 = (event: LambdaEvent) => {
+  if ('version' in event && event?.version === '2.0') {
+    if (
+      'http' in event.requestContext &&
+      event?.requestContext?.http?.method === 'POST' &&
+      'rawPath' in event &&
+      event?.rawPath?.startsWith('/RSC/')
+    ) {
+      const encodedRSCPath = encodeURIComponent(event.rawPath.substring(5));
+      const url = new URL(event.rawPath, `http://${event.headers.host}`);
+      url.pathname = `/RSC/${encodedRSCPath}`;
+      event.rawPath = url.pathname;
+      return event;
+    }
+  }
+  return event;
+};
+
+const honoHandler = handle(app);
+export const handler = (event: LambdaEvent, context: LambdaContext) => {
+  return honoHandler(fixHTTPAPIV2(event), context);
+};
