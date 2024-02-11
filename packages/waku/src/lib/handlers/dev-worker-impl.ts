ba37e57f8f937a4146844b68be20f60e151b233e
daishi <daishi@axlight.com>
2024-02-11 11:09:19 +0900

    externalizing waku

--- packages/waku/src/lib/handlers/dev-worker-impl.ts
+++ packages/waku/src/lib/handlers/dev-worker-impl.ts
@@ -143,15 +143,12 @@ const mergedViteConfig = await mergeUserViteConfig({
       externalConditions: ['react-server', 'workerd'],
     },
     external: [
-      // FIXME We want to externalize waku, but it fails on windows.
-      // 'waku',
-      // 'waku/client',
-      // 'waku/server',
-      // 'waku/router/client',
-      // 'waku/router/server',
+      'waku',
+      'waku/client',
+      'waku/server',
+      'waku/router/client',
+      'waku/router/server',
     ],
-    // FIXME We want to externalize waku, but it fails on windows.
-    noExternal: ['waku'],
   },
   appType: 'custom',
   server: { middlewareMode: true, hmr: { server: dummyServer } },
