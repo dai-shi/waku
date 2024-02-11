a601ef4bca590b96eedcc2655c9d8896c3cb6b46
Sophia Michelle Andren <20441876+sandren@users.noreply.github.com>
2024-02-10 21:58:43 -0800

    feat(demo): add inline query and reload button (#476)
    * feat(demo): add inline query and reload button
    
    * fix(demo): remove link
    
    * fix(demo): revise link
    
    ---------
    
    Co-authored-by: Daishi Kato <dai-shi@users.noreply.github.com>

--- examples/02_demo/src/components/header.tsx
+++ examples/02_demo/src/components/header.tsx
@@ -2,10 +2,18 @@ import { Link } from 'waku';
 
 export const Header = () => {
   return (
-    <header className="fixed left-0 top-0 p-6">
+    <header className="flex items-center gap-4 p-6 lg:fixed lg:left-0 lg:top-0">
       <h2 className="text-lg font-bold tracking-tight">
         <Link to="/">Waku pokemon</Link>
       </h2>
+      <a
+        href="https://github.com/dai-shi/waku/tree/main/examples/02_demo/src"
+        target="_blank"
+        rel="noreferrer"
+        className="text-sm hover:underline"
+      >
+        (source)
+      </a>
     </header>
   );
 };
