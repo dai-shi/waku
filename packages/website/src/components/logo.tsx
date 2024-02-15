91fb41175cdc92c38b0727a934968fd61131dba7
Sophia Michelle Andren <20441876+sandren@users.noreply.github.com>
2024-02-15 05:24:36 -0800

    feat(website): add popover menu (#489)
    * feat(website): add popover menu
    
    * feat(website): add outside click behavior
    
    * fix(website): z-index issue

--- packages/website/src/components/logo.tsx
+++ packages/website/src/components/logo.tsx
@@ -1,8 +1,13 @@
 'use client';
 
 import { useState } from 'react';
+import cx from 'clsx';
 
-export const Logo = () => {
+type LogoProps = {
+  className?: string;
+};
+
+export const Logo = ({ className = '' }: LogoProps) => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const handleContextMenu = (event: any) => {
@@ -22,7 +27,7 @@ export const Logo = () => {
         onContextMenu={handleContextMenu}
         src="https://cdn.candycode.com/waku/waku-logo-shadow.svg"
         alt="Waku"
-        className="block w-full max-w-[15rem] lg:max-w-[25rem]"
+        className={cx('block w-full max-w-[15rem] lg:max-w-[25rem]', className)}
       />
       <span className="sr-only">Waku</span>
       {isMenuOpen && (
@@ -34,7 +39,7 @@ export const Logo = () => {
             href="https://cdn.candycode.com/waku/waku-logo.zip"
             download={true}
             onClick={handleClose}
-            className="text-red-50 rounded-md bg-primary-700 px-6 py-3 text-xl font-black uppercase leading-none tracking-wide transition-colors duration-300 ease-in-out hover:bg-primary-500 focus:ring-4 focus:ring-primary-300 sm:px-8 sm:py-4 sm:text-2xl"
+            className="text-red-50 whitespace-nowrap rounded-md bg-primary-700 px-6 py-3 text-xl font-black uppercase leading-none tracking-wide transition-colors duration-300 ease-in-out hover:bg-primary-500 focus:ring-4 focus:ring-primary-300"
           >
             waku-logo.zip
           </a>
