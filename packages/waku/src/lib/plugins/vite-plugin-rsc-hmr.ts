5cc835518ddbd08ab22509371d10ed3fab995bbb
Mohammad Bagher Abiyat <zorofight94@gmail.com>
2024-02-15 17:45:45 +0330

    rely on httpServer

--- packages/waku/src/lib/plugins/vite-plugin-rsc-hmr.ts
+++ packages/waku/src/lib/plugins/vite-plugin-rsc-hmr.ts
@@ -10,6 +10,7 @@ import {
   fileURLToFilePath,
   decodeFilePathFromAbsolute,
 } from '../utils/path.js';
+import type { Server } from 'node:http';
 
 type ModuleImportResult = TransformResult & {
   id: string;
