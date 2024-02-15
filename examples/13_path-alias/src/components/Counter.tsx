590af288378af87b49dbca64dd96c07a4b3490e4
daishi <daishi@axlight.com>
2024-02-15 23:28:36 +0900

    wip: add common component

--- examples/13_path-alias/src/components/Counter.tsx
+++ examples/13_path-alias/src/components/Counter.tsx
@@ -2,11 +2,15 @@
 
 import { useState } from 'react';
 
+import { MyFragment } from '@/components/MyFragment.js';
+
 export const Counter = () => {
   const [count, setCount] = useState(0);
   return (
     <div style={{ border: '3px blue dashed', margin: '1em', padding: '1em' }}>
-      <p>Count: {count}</p>
+      <MyFragment>
+        <p>Count: {count}</p>
+      </MyFragment>
       <button onClick={() => setCount((c) => c + 1)}>Increment</button>
       <h3>This is a client component.</h3>
     </div>
