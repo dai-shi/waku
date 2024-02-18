15b2cb02cee66877625854102c6fc0bb00c77291
daishi <daishi@axlight.com>
2024-02-18 15:13:03 +0900

    custom 404 for netlify functions

--- packages/waku/src/lib/builder/serve-netlify.ts
+++ packages/waku/src/lib/builder/serve-netlify.ts
@@ -9,6 +9,12 @@ const env: Record<string, string> = process.env as any;
 
 const app = new Hono();
 app.use('*', honoMiddleware({ loadEntries, ssr, env }));
+app.notFound((c) => {
+  if ((globalThis as any).__WAKU_NOT_FOUND_HTML__) {
+    return c.html((globalThis as any).__WAKU_NOT_FOUND_HTML__, 404);
+  }
+  return c.text('404 Not Found', 404);
+});
 
 export default async (req: Request, context: Context) =>
   app.fetch(req, { context });
