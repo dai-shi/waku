18e56310bd7d6f163138189ce1f8ce42a8638a26
Daishi Kato <dai-shi@users.noreply.github.com>
2024-02-19 19:47:42 +0900

    support custom 404 (static html only) (#497)
    * custom 404 for waku cli
    
    * custom 404 for vercel serverless
    
    * fix path
    
    * custom 404 for netlify functions
    
    * custom 404 for cloudflare workers
    
    * custom 404 for deno deploy
    
    * refactor serve-netlify.ts
    
    * move 404.html to 03_minimal, because 01_template will have a better solution for 404 in the future.

--- packages/waku/src/lib/builder/serve-netlify.ts
+++ packages/waku/src/lib/builder/serve-netlify.ts
@@ -9,6 +9,13 @@ const env: Record<string, string> = process.env as any;
 
 const app = new Hono();
 app.use('*', honoMiddleware({ loadEntries, ssr, env }));
+app.notFound((c) => {
+  const notFoundHtml = (globalThis as any).__WAKU_NOT_FOUND_HTML__;
+  if (typeof notFoundHtml === 'string') {
+    return c.html(notFoundHtml, 404);
+  }
+  return c.text('404 Not Found', 404);
+});
 
 export default async (req: Request, context: Context) =>
   app.fetch(req, { context });
