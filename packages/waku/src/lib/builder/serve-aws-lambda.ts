d4939d2cd454db73f6d069651efbcac9b79d321d
Andreas Heissenberger <andreas@heissenberger.at>
2024-02-20 01:50:22 +0100

    feat: add 404.html support to aws-deploy (#511)
    * feat: add 404 to aws-deploy
    
    * fix liniting
    
    * Update packages/waku/src/lib/builder/serve-aws-lambda.ts
    
    * Update packages/waku/src/lib/builder/serve-aws-lambda.ts
    
    ---------
    
    Co-authored-by: Daishi Kato <dai-shi@users.noreply.github.com>

--- packages/waku/src/lib/builder/serve-aws-lambda.ts
+++ packages/waku/src/lib/builder/serve-aws-lambda.ts
@@ -3,10 +3,12 @@ import { handle } from 'hono/aws-lambda';
 import { serveStatic } from '@hono/node-server/serve-static';
 
 import { honoMiddleware } from '../middleware/hono-prd.js';
+import path from 'node:path';
+import { existsSync, readFileSync } from 'node:fs';
 
 const ssr = !!import.meta.env.WAKU_BUILD_SSR;
-const distDir = import.meta.env.WAKU_CONFIG_DIST_DIR;
-const publicDir = import.meta.env.WAKU_CONFIG_PUBLIC_DIR;
+const distDir = import.meta.env.WAKU_CONFIG_DIST_DIR!;
+const publicDir = import.meta.env.WAKU_CONFIG_PUBLIC_DIR!;
 const loadEntries = () => import(import.meta.env.WAKU_ENTRIES_FILE!);
 
 const env = process.env as Record<string, string>;
@@ -14,5 +16,12 @@ const env = process.env as Record<string, string>;
 const app = new Hono();
 app.use('*', serveStatic({ root: `${distDir}/${publicDir}` }));
 app.use('*', honoMiddleware({ loadEntries, ssr, env }));
+app.notFound(async (c) => {
+  const file = path.join(distDir, publicDir, '404.html');
+  if (existsSync(file)) {
+    return c.html(readFileSync(file, 'utf8'), 404);
+  }
+  return c.text('404 Not Found', 404);
+});
 
 export const handler = handle(app);
