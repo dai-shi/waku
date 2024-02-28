2bb47828a43c10840a5d88650e9bbcac31d006fe
Sunil Pai <threepointone@gmail.com>
2024-02-28 12:20:07 +0000

    feat: `--with-partykit`, 404 responses
    This serves `404.html`, if available in `/public`, for unmatched requests.
    
    (This also adds a bugfix for the generated `partykit.json` config file.)

--- packages/waku/src/lib/builder/serve-partykit.ts
+++ packages/waku/src/lib/builder/serve-partykit.ts
@@ -8,6 +8,24 @@ let serveWaku: ReturnType<typeof honoMiddleware> | undefined;
 
 const app = new Hono();
 app.use('*', (c, next) => serveWaku!(c, next));
+app.notFound(async (c) => {
+  // @ts-expect-error partykit's types aren't available
+  const assetsFetcher = c.env.assets as {
+    fetch(url: string): Promise<Response | undefined>;
+  };
+  // check if there's a 404.html in the static assets
+  const notFoundStaticAssetResponse = await assetsFetcher.fetch('/404.html');
+  // if there is, return it
+  if (notFoundStaticAssetResponse) {
+    return new Response(notFoundStaticAssetResponse.body, {
+      status: 404,
+      statusText: 'Not Found',
+      headers: notFoundStaticAssetResponse.headers,
+    });
+  }
+  // otherwise, return a simple 404 response
+  return c.text('404 Not Found', 404);
+});
 
 export default {
   onFetch(request: Request, lobby: any, ctx: Parameters<typeof app.fetch>[2]) {
