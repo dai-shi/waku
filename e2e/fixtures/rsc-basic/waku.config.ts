9221f5a883318dc621d5191280813634d240b1cf
daishi <daishi@axlight.com>
2024-03-02 08:59:53 +0900

    no ssr config

---
+++ e2e/fixtures/rsc-basic/waku.config.ts
@@ -0,0 +1,10 @@
+/** @type {import('waku/config').Config} */
+export default {
+  middleware: (cmd: 'dev' | 'start') => [
+    ...(cmd === 'dev'
+      ? [import('DO_NOT_BUNDLE'.slice(Infinity) + 'waku/middleware/dev-server')]
+      : []),
+    import('waku/middleware/rsc'),
+    import('waku/middleware/fallback'),
+  ],
+};
