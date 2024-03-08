d621df26fa91de06195d22bb46a1004fe607a2c2
daishi <daishi@axlight.com>
2024-03-08 12:20:52 +0900

    Revert "does it work without entries.tsx and main.tsx?"
    This reverts commit 757ddc764dd7aee58c355b1ccecef4aff15b175b.

---
+++ examples/10_fs-router/src/main.tsx
@@ -0,0 +1,19 @@
+import { StrictMode } from 'react';
+import { createRoot, hydrateRoot } from 'react-dom/client';
+import { Router } from 'waku/router/client';
+
+import { ErrorBoundary } from './components/ErrorBoundary.js';
+
+const rootElement = (
+  <StrictMode>
+    <ErrorBoundary fallback={(error) => <h1>{String(error)}</h1>}>
+      <Router />
+    </ErrorBoundary>
+  </StrictMode>
+);
+
+if (document.body.dataset.hydrate) {
+  hydrateRoot(document.body, rootElement);
+} else {
+  createRoot(document.body).render(rootElement);
+}
