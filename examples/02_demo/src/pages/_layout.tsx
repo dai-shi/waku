a00a2f6c4db69b79203a83e72a9f1416083f9e67
Sophia Michelle Andren <20441876+sandren@users.noreply.github.com>
2024-03-09 22:16:24 -0800

    feat(starter): switch to file-based router (#588)

--- examples/02_demo/src/pages/_layout.tsx
+++ examples/02_demo/src/pages/_layout.tsx
@@ -7,20 +7,26 @@ import { Footer } from '../components/footer.js';
 
 type RootLayoutProps = { children: ReactNode };
 
-export const RootLayout = async ({ children }: RootLayoutProps) => {
+export default async function RootLayout({ children }: RootLayoutProps) {
   const data = await getData();
 
   return (
-    <div className="font-['Nunito']">
+    <div className="font-nunito">
       <meta property="description" content={data.description} />
       <link rel="icon" type="image/png" href={data.icon} />
       <Header />
-      <main className="flex min-h-svh items-center justify-center *:min-h-64 *:min-w-64">
+      <main className="flex items-center justify-center lg:min-h-svh">
         {children}
       </main>
       <Footer />
     </div>
   );
+}
+
+export const getConfig = async () => {
+  return {
+    render: 'static',
+  };
 };
 
 const getData = async () => {
