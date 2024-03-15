04af0f2879e301bd6b1b3ea19c1c82e8aa689e13
Mohammad Bagher Abiyat <zorofight94@gmail.com>
2024-03-15 08:26:56 +0330

    debug missing root comp

--- packages/waku/src/lib/renderers/html-renderer.ts
+++ packages/waku/src/lib/renderers/html-renderer.ts
@@ -271,6 +271,7 @@ export const renderHtml = async (
                   fileURLToFilePath(import.meta.url),
                   '../../..',
                 );
+                cosnole.log('wakuDist', wakuDist, filePath)
                 if (filePath.startsWith(wakuDist)) {
                   const id =
                     'waku' +
