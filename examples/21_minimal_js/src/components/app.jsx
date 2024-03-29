2b260907f76c8f5624872e42ef3be708d5237ac5
daishi <daishi@axlight.com>
2024-03-29 10:11:50 +0900

    add not-yet-working example

---
+++ examples/21_minimal_js/src/components/app.jsx
@@ -0,0 +1,25 @@
+import { Suspense } from 'react';
+
+import { Counter } from './counter';
+
+const App = ({ name }) => {
+  return (
+    <div style={{ border: '3px red dashed', margin: '1em', padding: '1em' }}>
+      <title>Waku</title>
+      <h1>Hello {name}!!</h1>
+      <h3>This is a server component.</h3>
+      <Suspense fallback="Pending...">
+        <ServerMessage />
+      </Suspense>
+      <Counter />
+      <div>{new Date().toISOString()}</div>
+    </div>
+  );
+};
+
+const ServerMessage = async () => {
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+  return <p>Hello from server!</p>;
+};
+
+export default App;
