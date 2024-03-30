7ca0bffcb0d68c692d2a36c32c6afeb981f8c80a
daishi <daishi@axlight.com>
2024-03-30 22:07:21 +0900

    fix build

--- packages/waku/src/router/fs-router.ts
+++ packages/waku/src/router/fs-router.ts
@@ -6,7 +6,7 @@ const DO_NOT_BUNDLE = '';
 
 export function fsRouter(
   importMetaUrl: string,
-  loadPage: (file: string) => Promise<any>,
+  loadPage: (file: string) => Promise<any> | undefined,
   pages = 'pages',
 ) {
   return createPages(
@@ -35,16 +35,25 @@ export function fsRouter(
           recursive: true,
         });
         files = files.flatMap((file) => {
-          if (!EXTENSIONS.includes(extname(file))) {
+          const myExt = extname(file);
+          const myExtIndex = EXTENSIONS.indexOf(myExt);
+          if (myExtIndex === -1) {
             return [];
           }
-          // HACK: replace "_slug_" to "[slug]"
+          // HACK: replace "_slug_" to "[slug]" for build
           file = file.replace(/(^|\/|\\)_(\w+)_(\/|\\|\.)/g, '$1[$2]$3');
-          if (sep === '/') {
-            return [file];
-          }
           // For Windows
-          return [file.replace(/\\/g, '/')];
+          file = sep === '/' ? file : file.replace(/\\/g, '/');
+          // HACK: resolve different extensions for build
+          const exts = [myExt, ...EXTENSIONS];
+          exts.splice(myExtIndex + 1, 1); // remove the second myExt
+          for (const ext of exts) {
+            const f = file.slice(0, -myExt.length) + ext;
+            if (loadPage(f)) {
+              return [f];
+            }
+          }
+          throw new Error('Failed to resolve ' + file);
         });
       }
       for (const file of files) {
