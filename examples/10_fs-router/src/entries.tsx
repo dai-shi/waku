d46091b487d56b77342a6d32fc13f18780d6637e
Daishi Kato <dai-shi@users.noreply.github.com>
2024-03-30 18:18:17 +0900

    refactor: use import.meta.glob (#635)
    Nobody told me that it was possible. 😄

--- examples/10_fs-router/src/entries.tsx
+++ examples/10_fs-router/src/entries.tsx
@@ -2,20 +2,14 @@ import { fsRouter } from 'waku/router/server';
 
 export default fsRouter(import.meta.url, loader);
 
-function loader(dir: string, file: string) {
-  const p = file.replace(/\.\w+$/, '').split('/');
-  switch (p.length) {
-    case 1:
-      return import(`./${dir}/${p[0]}.tsx`);
-    case 2:
-      return import(`./${dir}/${p[0]}/${p[1]}.tsx`);
-    case 3:
-      return import(`./${dir}/${p[0]}/${p[1]}/${p[2]}.tsx`);
-    case 4:
-      return import(`./${dir}/${p[0]}/${p[1]}/${p[2]}/${p[3]}.tsx`);
-    case 5:
-      return import(`./${dir}/${p[0]}/${p[1]}/${p[2]}/${p[3]}/${p[5]}.tsx`);
-    default:
-      throw new Error('too deep');
+declare global {
+  interface ImportMeta {
+    readonly glob: any;
   }
+}
+
+function loader(dir: string, file: string) {
+  const fname = `./${dir}/${file.replace(/\.\w+$/, '')}.tsx`;
+  const modules = import.meta.glob('./pages/**/*.tsx');
+  return modules[fname]();
 }
