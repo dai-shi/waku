adfa8264dca25576f8155beda4065ebcea30ee7b
daishi <daishi@axlight.com>
2024-03-31 10:15:17 +0900

    remove error boundary in examples

--- examples/10_fs-router/src/main.tsx
+++ examples/10_fs-router/src/main.tsx
@@ -2,13 +2,9 @@ import { StrictMode } from 'react';
 import { createRoot, hydrateRoot } from 'react-dom/client';
 import { Router } from 'waku/router/client';
 
-import { ErrorBoundary } from './components/ErrorBoundary';
-
 const rootElement = (
   <StrictMode>
-    <ErrorBoundary fallback={(error) => <h1>{String(error)}</h1>}>
-      <Router />
-    </ErrorBoundary>
+    <Router />
   </StrictMode>
 );
 
