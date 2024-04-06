775bc12dca0e66864b29e412d0ce90156588fda2
daishi <daishi@axlight.com>
2024-04-06 20:12:49 +0900

    wip: add debug log

--- e2e/utils.ts
+++ e2e/utils.ts
@@ -56,6 +56,7 @@ export function debugChildProcess(
       file: sourceFile,
     });
     console.error(`stderr: ${str}`);
+    console.error(`sourceFile: ${sourceFile}`, ignoreErrors);
   });
 }
 
