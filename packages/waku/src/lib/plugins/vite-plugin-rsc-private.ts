182c162ade06887a660e8271c7cbce7e44fac51e
Mohammad Bagher Abiyat <zorofight94@gmail.com>
2024-04-27 05:13:59 +0330

    fix: private dir hmr (#675)
    Resolves #592 
    
    We did not react to changes in the private dir. 
    
    Since we're sure they're server only, let's treat them as rsc updates,
    so the changes get reflected on the client.
    
    ---------
    
    Co-authored-by: Daishi Kato <dai-shi@users.noreply.github.com>

--- packages/waku/src/lib/plugins/vite-plugin-rsc-private.ts
+++ packages/waku/src/lib/plugins/vite-plugin-rsc-private.ts
@@ -1,11 +1,14 @@
 import type { Plugin } from 'vite';
 
 import { joinPath } from '../utils/path.js';
+import type { HotUpdatePayload } from './vite-plugin-rsc-hmr.js';
 
 export function rscPrivatePlugin({
   privateDir,
+  hotUpdateCallback,
 }: {
   privateDir: string;
+  hotUpdateCallback?: (payload: HotUpdatePayload) => void;
 }): Plugin {
   let privatePath: string;
   return {
@@ -16,6 +19,11 @@ export function rscPrivatePlugin({
     load(id) {
       if (id.startsWith(privatePath)) {
         throw new Error('Private file access is not allowed');
+      }
+    },
+    handleHotUpdate({ file }) {
+      if (file.startsWith(privatePath)) {
+        hotUpdateCallback?.({ type: 'custom', event: 'rsc-reload' });
       }
     },
   };
