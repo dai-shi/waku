ae0b1de90c4090858701b53bd57b9e7c2fbe1945
Daishi Kato <dai-shi@users.noreply.github.com>
2024-05-03 15:41:28 +0900

    Avoid exposing configs that are not configurable (#682)
    supersedes #679
    
    Implementation-wise, there are many hacks. We should improve it later.

--- packages/waku/src/lib/builder/output-vercel.ts
+++ packages/waku/src/lib/builder/output-vercel.ts
@@ -2,14 +2,16 @@ import path from 'node:path';
 import { cpSync, existsSync, mkdirSync, writeFileSync } from 'node:fs';
 
 import type { ResolvedConfig } from '../config.js';
+import { DIST_PUBLIC } from './constants.js';
 
 // https://vercel.com/docs/build-output-api/v3
 export const emitVercelOutput = async (
   rootDir: string,
   config: ResolvedConfig,
+  serveJs: string,
   type: 'static' | 'serverless',
 ) => {
-  const publicDir = path.join(rootDir, config.distDir, config.publicDir);
+  const publicDir = path.join(rootDir, config.distDir, DIST_PUBLIC);
   const outputDir = path.resolve('.vercel', 'output');
   cpSync(publicDir, path.join(outputDir, 'static'), { recursive: true });
 
@@ -37,7 +39,7 @@ export const emitVercelOutput = async (
     }
     const vcConfigJson = {
       runtime: 'nodejs20.x',
-      handler: `${config.distDir}/${config.serveJs}`,
+      handler: `${config.distDir}/${serveJs}`,
       launcherType: 'Nodejs',
     };
     writeFileSync(
