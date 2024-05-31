d58db91fc272e965defefc2b77eb389724b5897f
daishi <daishi@axlight.com>
2024-05-31 10:36:30 +0900

    inline action example

--- examples/11_form/src/components/ServerForm.tsx
+++ examples/11_form/src/components/ServerForm.tsx
@@ -4,11 +4,24 @@ async function requestUsername(formData: FormData) {
   console.log(`username: ${username}`);
 }
 
+// FIXME make this example more realistic
 export const ServerForm = () => {
   return (
-    <form action={requestUsername}>
-      <input type="text" name="username" />
-      <button type="submit">Request</button>
-    </form>
+    <>
+      <form action={requestUsername}>
+        <input type="text" name="username" />
+        <button type="submit">Request</button>
+      </form>
+      <form
+        action={async (formData: FormData) => {
+          'use server';
+          const hobby = formData.get('hobby');
+          console.log(`hobby: ${hobby}`);
+        }}
+      >
+        <input type="text" name="hobby" />
+        <button type="submit">Request</button>
+      </form>
+    </>
   );
 };
