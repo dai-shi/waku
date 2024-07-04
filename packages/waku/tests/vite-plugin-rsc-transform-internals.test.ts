21fcbe81de112fca6478c2ffe9d30dc6afb1a9b1
Alex Yang <himself65@outlook.com>
2024-07-03 21:00:18 -0700

    fix: server action cases (#774)
    Fix some RSC case, will fix https://github.com/dai-shi/waku/pull/745. In
    the future, I will migrate the logic to
    https://github.com/himself65/react-server and keep same output
    
    Some issues still happening:
    
    1. `export default async function () {"use server"}` cannot be RSCA
    2. `export function a() { "use server" }` should throw error, because
    it's not async
    
    ---------
    
    Co-authored-by: daishi <daishi@axlight.com>

--- packages/waku/tests/vite-plugin-rsc-transform-internals.test.ts
+++ packages/waku/tests/vite-plugin-rsc-transform-internals.test.ts
@@ -53,16 +53,113 @@ export const log = (mesg) => {
 `;
     expect(await transform(code, '/src/App.tsx', { ssr: true }))
       .toMatchInlineSnapshot(`
-        "
-        import { registerServerReference as __waku_registerServerReference } from 'react-server-dom-webpack/server.edge';
-        ;
-
-        export const log = (mesg) => {
-          console.log(mesg);
+        "import { registerServerReference as __waku_registerServerReference } from 'react-server-dom-webpack/server.edge';
+        export const log = (mesg)=>{
+            console.log(mesg);
         };
+        if (typeof log === "function") {
+            __waku_registerServerReference(log, "/src/App.tsx", "log");
+        }
+        "
+      `);
+  });
 
-        if (typeof log === 'function') {
-          __waku_registerServerReference(log, '/src/App.tsx', 'log');
+  test('server action in object', async () => {
+    const code = `
+import type { ReactNode } from 'react';
+import { createAI } from 'ai/rsc';
+
+const AI = createAI({
+  actions: {
+    foo: async () => {
+      'use server';
+      return 0;
+    },
+  },
+});
+
+export function ServerProvider({ children }: { children: ReactNode }) {
+  return <AI>{children}</AI>;
+}
+`;
+    expect(await transform(code, '/src/App.tsx', { ssr: true }))
+      .toMatchInlineSnapshot(`
+        "import type { ReactNode } from 'react';
+        import { createAI } from 'ai/rsc';
+        import { registerServerReference as __waku_registerServerReference } from 'react-server-dom-webpack/server.edge';
+        export const __waku_action1 = __waku_registerServerReference(async ()=>{
+            return 0;
+        }, "/src/App.tsx", "__waku_action1");
+        const AI = createAI({
+            actions: {
+                foo: __waku_action1.bind(null)
+            }
+        });
+        export function ServerProvider({ children }: {
+            children: ReactNode;
+        }) {
+            return <AI>{children}</AI>;
+        }
+        "
+      `);
+  });
+
+  test('create server with bind', async () => {
+    const code = `
+'use server';
+import { InternalProvider } from './shared.js';
+import { jsx } from 'react/jsx-runtime';
+
+async function innerAction({ action }, ...args) {
+  'use server';
+  return await action(...args);
+}
+
+function wrapAction(action) {
+  return innerAction.bind(null, { action });
+}
+
+export function createAI({ actions }) {
+  const wrappedActions = {};
+  for (const name in actions) {
+    wrappedActions[name] = wrapAction(actions[name]);
+  }
+  return function AI(props) {
+    return jsx(InternalProvider, {
+      actions: wrappedActions,
+      children: props.children,
+    });
+  };
+}`;
+    expect(await transform(code, '/src/App.tsx', { ssr: true }))
+      .toMatchInlineSnapshot(`
+        "import { InternalProvider } from './shared.js';
+        import { jsx } from 'react/jsx-runtime';
+        import { registerServerReference as __waku_registerServerReference } from 'react-server-dom-webpack/server.edge';
+        export async function __waku_action1({ action }, ...args) {
+            return await action(...args);
+        }
+        __waku_registerServerReference(__waku_action1, "/src/App.tsx", "__waku_action1");
+        const innerAction = __waku_action1.bind(null);
+        function wrapAction(action) {
+            return innerAction.bind(null, {
+                action
+            });
+        }
+        export function createAI({ actions }) {
+            const wrappedActions = {};
+            for(const name in actions){
+                wrappedActions[name] = wrapAction(actions[name]);
+            }
+            return function AI(props) {
+                return jsx(InternalProvider, {
+                    actions: wrappedActions,
+                    children: props.children
+                });
+            };
+        }
+        if (typeof createAI === "function") {
+            __waku_registerServerReference(createAI, "/src/App.tsx", "createAI");
         }
         "
       `);
@@ -71,7 +168,7 @@ export const log = (mesg) => {
   test('inline use server (function declaration)', async () => {
     const code = `
 export default function App({ a }) {
-  function log(mesg) {
+  async function log(mesg) {
     'use server';
     console.log(mesg, a);
   }
@@ -80,17 +177,17 @@ export default function App({ a }) {
 `;
     expect(await transform(code, '/src/App.tsx', { ssr: true }))
       .toMatchInlineSnapshot(`
-        "import { registerServerReference as __waku_registerServerReference } from 'react-server-dom-webpack/server.edge';
-        export function __waku_action1(a, mesg) {
-            console.log(mesg, a);
-        }
-        __waku_registerServerReference(__waku_action1, "/src/App.tsx", "__waku_action1");
-        export default function App({ a }) {
-            const log = __waku_action1.bind(null, a);
-            return <Hello log={log}/>;
-        }
-        "
-      `);
+      "import { registerServerReference as __waku_registerServerReference } from 'react-server-dom-webpack/server.edge';
+      export async function __waku_action1(a, mesg) {
+          console.log(mesg, a);
+      }
+      __waku_registerServerReference(__waku_action1, "/src/App.tsx", "__waku_action1");
+      export default function App({ a }) {
+          const log = __waku_action1.bind(null, a);
+          return <Hello log={log}/>;
+      }
+      "
+    `);
   });
 
   test('inline use server (const function expression)', async () => {
@@ -148,7 +245,7 @@ export default function App() {
   test('inline use server (in an object)', async () => {
     const code = `
 const actions = {
-  log: (mesg) => {
+  log: async (mesg) => {
     'use server';
     console.log(mesg);
   },
@@ -160,7 +257,7 @@ export default function App() {
     expect(await transform(code, '/src/App.tsx', { ssr: true }))
       .toMatchInlineSnapshot(`
         "import { registerServerReference as __waku_registerServerReference } from 'react-server-dom-webpack/server.edge';
-        export const __waku_action1 = __waku_registerServerReference((mesg)=>{
+        export const __waku_action1 = __waku_registerServerReference(async (mesg)=>{
             console.log(mesg);
         }, "/src/App.tsx", "__waku_action1");
         const actions = {
