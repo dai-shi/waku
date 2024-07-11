e660f9df3a71f4bbcdd40a08fe188e73e6087ac8
Alex Yang <himself65@outlook.com>
2024-07-10 20:08:53 -0700

    fix: second try

--- packages/waku/src/lib/middleware/dev-server.ts
+++ packages/waku/src/lib/middleware/dev-server.ts
@@ -11,7 +11,6 @@ import {
   fileURLToFilePath,
   encodeFilePathToAbsolute,
   decodeFilePathFromAbsolute,
-  filePathToFileURL,
 } from '../utils/path.js';
 import { patchReactRefresh } from '../plugins/patch-react-refresh.js';
 import { nonjsResolvePlugin } from '../plugins/vite-plugin-nonjs-resolve.js';
@@ -311,7 +310,7 @@ export const devServer: Middleware = (options) => {
   const configPromise = resolveConfig(options.config);
 
   (globalThis as any).__WAKU_HACK_IMPORT__ = async (id: string) =>
-    loadServerFileRsc(filePathToFileURL(id));
+    loadServerFileRsc(id);
 
   const {
     vitePromise,
