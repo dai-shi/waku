f30c19ca61412138983f50c84d97dc8e4aa7d1f9
daishi <daishi@axlight.com>
2024-07-14 20:43:19 +0900

    fix: import meta env

--- packages/waku/src/lib/middleware/dev-server.ts
+++ packages/waku/src/lib/middleware/dev-server.ts
@@ -2,14 +2,14 @@ import type { Middleware } from './types.js';
 
 declare global {
   interface ImportMeta {
-    readonly env: Record<string, string>;
+    readonly env: Record<string, string> | undefined;
   }
 }
 
 const DO_NOT_BUNDLE = '';
 
 export const devServer: Middleware = (options) => {
-  if (import.meta.env.MODE === 'production') {
+  if (import.meta.env && import.meta.env.MODE === 'production') {
     // pass through
     return (_ctx, next) => next();
   }
