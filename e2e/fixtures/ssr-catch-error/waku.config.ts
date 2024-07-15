f61b264a466bec88c3c3bfcc5588a83bd39ea4c9
Daishi Kato <dai-shi@users.noreply.github.com>
2024-07-15 10:29:00 +0900

    fix: hide DO_NOT_BUNDLE (#792)
    Hope this works.

--- e2e/fixtures/ssr-catch-error/waku.config.ts
+++ e2e/fixtures/ssr-catch-error/waku.config.ts
@@ -1,15 +1,7 @@
-const DO_NOT_BUNDLE = '';
-
 /** @type {import('waku/config').Config} */
 export default {
-  middleware: (cmd: 'dev' | 'start') => [
-    ...(cmd === 'dev'
-      ? [
-          import(
-            /* @vite-ignore */ DO_NOT_BUNDLE + 'waku/middleware/dev-server'
-          ),
-        ]
-      : []),
+  middleware: () => [
+    import('waku/middleware/dev-server'),
     import('./src/middleware/validator.js'),
     import('waku/middleware/rsc'),
     import('waku/middleware/fallback'),
