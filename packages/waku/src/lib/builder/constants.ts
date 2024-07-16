e4a73efcc62d2209918cf36c961d09ee2fd025ab
jkhaui <jordan.lee.163@gmail.com>
2024-07-17 09:12:22 +1000

    refactor: move _routes.json logic to output-cloudflare.ts

--- packages/waku/src/lib/builder/constants.ts
+++ packages/waku/src/lib/builder/constants.ts
@@ -2,7 +2,7 @@
 // We may change this in the future
 export const DIST_ENTRIES_JS = 'entries.js';
 export const DIST_SERVE_JS = 'serve.js';
-export const DIST_WORKER_JS = '_worker.js';
+export const DIST_WORKER_JS = '_worker.js'; // for CloudFlare Pages
 export const DIST_PUBLIC = 'public';
 export const DIST_ASSETS = 'assets';
 export const DIST_SSR = 'ssr';
--- packages/waku/src/lib/builder/output-cloudflare.ts
+++ packages/waku/src/lib/builder/output-cloudflare.ts
@@ -1,14 +1,28 @@
 import path from 'node:path';
 import { existsSync, writeFileSync } from 'node:fs';
+import { joinPath } from '../utils/path.js';
+import { DIST_ASSETS, DIST_PUBLIC } from './constants.js';
 
 import type { ResolvedConfig } from '../config.js';
-import { DIST_PUBLIC } from './constants.js';
 
-// XXX this can be very limited. FIXME if anyone has better knowledge.
 export const emitCloudflareOutput = async (
   rootDir: string,
   config: ResolvedConfig
 ) => {
+  const routesJsonPublicFile = joinPath(rootDir, config.srcDir, DIST_PUBLIC);
+  if (!existsSync(routesJsonPublicFile)) {
+    const routesJsonDistFile = joinPath(rootDir,
+      config.distDir,
+      '_routes.json');
+    const staticRoutes = {
+      version: 1,
+      include: ['/*'],
+      exclude: [`/${DIST_PUBLIC}/${DIST_ASSETS}/*`]
+    };
+
+    writeFileSync(routesJsonDistFile, JSON.stringify(staticRoutes));
+  }
+
   const wranglerTomlFile = path.join(rootDir, 'wrangler.toml');
   if (!existsSync(wranglerTomlFile)) {
     writeFileSync(
