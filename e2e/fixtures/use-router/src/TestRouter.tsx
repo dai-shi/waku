4823571885a48f339904d823faf552d20a8132b5
Philipp Melab <philipp.melab@gmail.com>
2024-07-25 09:10:36 +0200

    fix: adapt test case

--- e2e/fixtures/use-router/src/TestRouter.tsx
+++ e2e/fixtures/use-router/src/TestRouter.tsx
@@ -4,11 +4,7 @@ import { Link, useRouter_UNSTABLE } from 'waku';
 
 export default function TestRouter() {
   const router = useRouter_UNSTABLE();
-  // TODO: Why is this necessary? On the client `searchParams`
-  //       is a URLSearchParams object, but on the server it's an array.
-  //       Without explicitly converting it to a URLSearchParams object,
-  //       the code will break on the server because 'get' is not a function.
-  const params = router.searchParams;
+  const params = new URLSearchParams(router.query);
   const queryCount = parseInt(params.get('count') || '0');
   const hashCount = parseInt(router.hash?.substr(1) || '0');
   return (
