6e999fc08379ccedba0a5e8fb5563391f404fa3b
daishi <daishi@axlight.com>
2024-08-13 18:04:32 +0900

    fix layout

--- e2e/fixtures/vercel-ai/src/pages/_layout.tsx
+++ e2e/fixtures/vercel-ai/src/pages/_layout.tsx
@@ -10,16 +10,22 @@ type RootLayoutProps = { children: ReactNode };
 export default async function RootLayout({ children }: RootLayoutProps) {
   const data = await getData();
   return (
-    <div className="font-sans">
-      <meta property="description" content={data.description} />
-      <link rel="icon" type="image/png" href={data.icon} />
-      <TooltipProvider>
-        <Header />
-        <main className="bg-muted/50 dark:bg-background flex flex-1 flex-col">
-          {children}
-        </main>
-      </TooltipProvider>
-    </div>
+    <html>
+      <head>
+        <meta property="description" content={data.description} />
+        <link rel="icon" type="image/png" href={data.icon} />
+      </head>
+      <body>
+        <div className="font-sans">
+          <TooltipProvider>
+            <Header />
+            <main className="bg-muted/50 dark:bg-background flex flex-1 flex-col">
+              {children}
+            </main>
+          </TooltipProvider>
+        </div>
+      </body>
+    </html>
   );
 }
 
