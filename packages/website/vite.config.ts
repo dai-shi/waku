88ded1ddf2b4dbf5ff9fde35fe0f323b05f7aa79
daishi <daishi@axlight.com>
2024-08-20 10:28:33 +0900

    fix(dev): optimize deps

--- packages/website/vite.config.ts
+++ packages/website/vite.config.ts
@@ -2,6 +2,9 @@
 export default ({ mode }: { mode: string }) => {
   if (mode === 'development') {
     return {
+      optimizeDeps: {
+        include: ['tailwindcss/colors'],
+      },
       ssr: {
         optimizeDeps: {
           include: ['next-mdx-remote/rsc'],
