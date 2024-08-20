b57c3e05c8d5674d03ab38feed1093a91fedda8e
daishi <daishi@axlight.com>
2024-08-20 17:05:44 +0900

    chore(examples): add bind usage

--- examples/23_actions/src/components2/funcs2.ts
+++ examples/23_actions/src/components2/funcs2.ts
@@ -4,6 +4,11 @@ import { unstable_getCustomContext as getCustomContext } from 'waku/server';
 
 export const greet = async (name: string) => {
   await Promise.resolve();
-  console.log('Custom Context:', getCustomContext()); // ---> {}
   return `Hello ${name} from server!`;
+};
+
+export const hello = async (name: string) => {
+  await Promise.resolve();
+  console.log('Custom Context:', getCustomContext()); // ---> {}
+  console.log('Hello', name, '!');
 };
