242658a6ed03ad725a2242ae2e9f27f413173a47
Tyler <26290074+thegitduck@users.noreply.github.com>
2024-09-12 18:49:07 -0700

    feat: infer types for waku router

---
+++ packages/waku/src/router/base-types.ts
@@ -0,0 +1,6 @@
+export type { PathsForPages } from './create-pages-utils/inferred-path-types.js';
+
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
+export interface RouteConfig {
+  // routes to be overridden by users
+}
