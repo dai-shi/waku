42705d3458566987b9e0b7a4a9dd069b2e15a14e
Rob Marscher <rob@robmarscher.com>
2024-10-01 22:45:18 -0400

    Unit tests for vite-plugin-fs-router-typegen supporting functions

---
+++ packages/waku/tests/vite-plugin-fs-router-typegen.test.ts
@@ -0,0 +1,33 @@
+import { describe, expect, test } from 'vitest';
+
+import {
+  getImportModuleNames,
+  toIdentifier,
+} from '../src/lib/plugins/vite-plugin-fs-router-typegen.js';
+
+describe('vite-plugin-fs-router-typegen', () => {
+  test('generates valid module names for fs entries', async () => {
+    expect(toIdentifier('/_layout.tsx')).toBe('Layout');
+    expect(toIdentifier('/[category]/[...tags]/index.tsx')).toBe(
+      'CategoryTagsIndex',
+    );
+  });
+
+  test('allows unicode characters in module names', async () => {
+    expect(toIdentifier('/øné_two_three.tsx')).toBe('ØnéTwoThree');
+  });
+
+  test('handles collisions of fs entry module names', async () => {
+    expect(
+      getImportModuleNames([
+        '/one-two-three.tsx',
+        '/one_two_three.tsx',
+        '/one__two_three.tsx',
+      ]),
+    ).toEqual({
+      '/one-two-three.tsx': 'OneTwoThree',
+      '/one_two_three.tsx': 'OneTwoThree_b7ec48bc',
+      '/one__two_three.tsx': 'OneTwoThree_fb958a64',
+    });
+  });
+});
