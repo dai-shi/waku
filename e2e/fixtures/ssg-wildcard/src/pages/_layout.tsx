929ccef51e98cf479fb02bdbb6789697b5e8a528
Tyler <26290074+thegitduck@users.noreply.github.com>
2024-10-06 11:32:05 -0700

    fix: from first review

--- e2e/fixtures/ssg-wildcard/src/pages/_layout.tsx
+++ e2e/fixtures/ssg-wildcard/src/pages/_layout.tsx
@@ -1,5 +1,10 @@
 import type { ReactNode } from 'react';
 
-const Layout = ({ children }: { children: ReactNode }) => <div>{children}</div>;
+const Layout = ({ children }: { children: ReactNode }) => (
+  <div>
+    <title>Waku</title>
+    {children}
+  </div>
+);
 
 export default Layout;
