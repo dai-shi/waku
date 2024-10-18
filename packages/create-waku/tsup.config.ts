f09715b1c75ce4d233db00281e5c6417902761ae
Tyler <tyler@secondspectrum.com>
2024-10-18 12:10:54 -0400

    fix: trying banner to fix dynamix require issue
    from https://github.com/egoist/tsup/issues/927#issuecomment-2354939322

--- packages/create-waku/tsup.config.ts
+++ packages/create-waku/tsup.config.ts
@@ -4,4 +4,7 @@ export default defineConfig((options) => ({
   entry: ['src/index.ts'],
   format: ['esm'],
   minify: !options.watch,
+  banner: {
+    js: `import { createRequire } from 'module'; const require = createRequire(import.meta.url);`,
+  },
 }));
