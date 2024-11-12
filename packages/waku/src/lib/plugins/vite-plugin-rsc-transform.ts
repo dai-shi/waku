98536ad479ca7a316082a5e08b4d91d680d994d2
daishi <daishi@axlight.com>
2024-11-12 20:38:26 +0900

    Revert "not confident tho"
    This reverts commit 375d6ed80ce2084f8313f1d0f753fdf3e8bcaaa7.

--- packages/waku/src/lib/plugins/vite-plugin-rsc-transform.ts
+++ packages/waku/src/lib/plugins/vite-plugin-rsc-transform.ts
@@ -669,7 +669,7 @@ export function rscTransformPlugin(
       if (srcId.startsWith(wakuDist)) {
         srcId = 'waku' + srcId.slice(wakuDist.length).replace(/\.\w+$/, '');
       }
-      if (resolved && resolved.id !== srcId && !srcId.startsWith('/')) {
+      if (resolved && resolved.id !== srcId) {
         const map = options?.ssr ? resolvedMapSsr : resolvedMap;
         if (!map.has(resolved.id)) {
           map.set(resolved.id, srcId);
