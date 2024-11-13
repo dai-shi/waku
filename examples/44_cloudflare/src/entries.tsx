33bea3d603067c5bc0b64a9db78a41c3a5aff800
Rob Marscher <rob@robmarscher.com>
2024-11-12 19:49:15 -0500

    docs: Cloudflare guide update with example (#943)
    Moving a code example for Cloudflare to Waku so we don't need to
    reference my public gist and it's easier for everyone to test.

---
+++ examples/44_cloudflare/src/entries.tsx
@@ -0,0 +1,18 @@
+import { new_defineEntries } from 'waku/minimal/server';
+import { Slot } from 'waku/minimal/client';
+
+import App from './components/App';
+
+export default new_defineEntries({
+  unstable_handleRequest: async (input, { renderRsc, renderHtml }) => {
+    if (input.type === 'component') {
+      return renderRsc({ App: <App name={input.rscPath || 'Waku'} /> });
+    }
+    if (input.type === 'custom' && input.pathname === '/') {
+      return renderHtml({ App: <App name="Waku" /> }, <Slot id="App" />, '');
+    }
+  },
+  unstable_getBuildConfig: async () => [
+    { pathSpec: [], entries: [{ rscPath: '' }] },
+  ],
+});
