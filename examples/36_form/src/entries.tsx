97b6a0810d75cd2b9e4a8f3baa132aef887b0801
daishi <daishi@axlight.com>
2024-11-19 11:36:40 +0900

    chore 36_form

--- examples/36_form/src/entries.tsx
+++ examples/36_form/src/entries.tsx
@@ -1,26 +1,29 @@
-import { defineEntries } from 'waku/server';
-import { Slot } from 'waku/client';
-import App from './components/App';
+import { new_defineEntries } from 'waku/minimal/server';
+import { Slot } from 'waku/minimal/client';
 
-export default defineEntries(
-  // renderEntries
-  async (rscPath) => {
-    return {
-      App: <App name={rscPath || 'Waku'} />,
-    };
-  },
-  // getBuildConfig
-  async () => [{ pathname: '/', entries: [{ rscPath: '' }] }],
-  // getSsrConfig
-  async (pathname) => {
-    switch (pathname) {
-      case '/':
-        return {
-          rscPath: '',
-          html: <Slot id="App" />,
-        };
-      default:
-        return null;
+import App from './components/App';
+import { runWithRerender } from './als';
+
+export default new_defineEntries({
+  unstable_handleRequest: async (input, { renderRsc, renderHtml }) => {
+    if (input.type === 'component') {
+      return renderRsc({ App: <App name={input.rscPath || 'Waku'} /> });
+    }
+    if (input.type === 'function') {
+      const elements: Record<string, unknown> = {};
+      const rerender = (rscPath: string) => {
+        elements.App = <App name={rscPath || 'Waku'} />;
+      };
+      const value = await runWithRerender(rerender, () =>
+        input.fn(...input.args),
+      );
+      return renderRsc({ ...elements, _value: value });
+    }
+    if (input.type === 'custom' && input.pathname === '/') {
+      return renderHtml({ App: <App name="Waku" /> }, <Slot id="App" />, '');
     }
   },
-);
+  unstable_getBuildConfig: async () => [
+    { pathSpec: [], entries: [{ rscPath: '' }] },
+  ],
+});
