1089c2a2377a6b9daaaeea87329ff1e909bef2e4
daishi <daishi@axlight.com>
2024-11-29 19:37:43 +0900

    fix delegate plugin

--- packages/waku/src/lib/plugins/vite-plugin-rsc-delegate.ts
+++ packages/waku/src/lib/plugins/vite-plugin-rsc-delegate.ts
@@ -77,10 +77,6 @@ export function rscDelegatePlugin(
     },
     async handleHotUpdate(ctx) {
       if (mode === 'development') {
-        if (ctx.file.endsWith('/pages.gen.ts')) {
-          // auto generated file by fsRouterTypegenPlugin
-          return [];
-        }
         await updateAllStyles(); // FIXME is this too aggressive?
         if (moduleImports.has(ctx.file)) {
           // re-inject
@@ -100,6 +96,7 @@ export function rscDelegatePlugin(
           callback({ type: 'custom', event: 'rsc-reload' });
         }
       }
+      return [];
     },
     async transform(code, id) {
       // id can contain query string with vite deps optimization
