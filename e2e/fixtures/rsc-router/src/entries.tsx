3e054f787ee7df5dd4046087347f88028fbb83a4
daishi <daishi@axlight.com>
2024-12-08 08:58:47 +0900

    chore(e2e): migrate to new_defineRouter

--- e2e/fixtures/rsc-router/src/entries.tsx
+++ e2e/fixtures/rsc-router/src/entries.tsx
@@ -1,29 +1,62 @@
-import { unstable_defineRouter } from 'waku/router/server';
+import type { ReactNode } from 'react';
+import { new_defineRouter } from 'waku/router/server';
+import { Slot, Children } from 'waku/minimal/client';
+
+import Layout from './routes/layout.js';
+import Page from './routes/page.js';
+import FooPage from './routes/foo/page.js';
 
 const STATIC_PATHS = ['/', '/foo'];
+const PATH_PAGE: Record<string, ReactNode> = {
+  '/': <Page />,
+  '/foo': <FooPage />,
+};
 
-export default unstable_defineRouter(
-  // getPathConfig
-  async () =>
+export default new_defineRouter({
+  getPathConfig: async () =>
     STATIC_PATHS.map((path) => ({
       pattern: `^${path}$`,
       path: path
         .split('/')
         .filter(Boolean)
         .map((name) => ({ type: 'literal', name })),
-      isStatic: true,
+      routeElement: { isStatic: true },
+      elements: {
+        root: { isStatic: true },
+        'layout:/': { isStatic: true },
+        [`page:${path}`]: { isStatic: true },
+      },
     })),
-  // getComponent (id is "**/layout" or "**/page")
-  async (id) => {
-    switch (id) {
-      case 'layout':
-        return (await import('./routes/layout.js')).default;
-      case 'page':
-        return (await import('./routes/page.js')).default;
-      case 'foo/page':
-        return (await import('./routes/foo/page.js')).default;
-      default:
-        return null;
+  renderRoute: async (path) => {
+    if (!STATIC_PATHS.includes(path)) {
+      throw new Error('renderRoute: No such path:' + path);
     }
+    return {
+      routeElement: (
+        <Slot id="root">
+          <Slot id="layout:/">
+            <Slot id={`page:${path}`} />
+          </Slot>
+        </Slot>
+      ),
+      elements: {
+        root: (
+          <html>
+            <head>
+              <title>Waku example</title>
+            </head>
+            <body>
+              <Children />
+            </body>
+          </html>
+        ),
+        'layout:/': (
+          <Layout>
+            <Children />
+          </Layout>
+        ),
+        [`page:${path}`]: PATH_PAGE[path],
+      },
+    };
   },
-);
+});
