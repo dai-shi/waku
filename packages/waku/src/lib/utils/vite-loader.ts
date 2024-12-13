c53287f735d0d160eda7ad3c65df3db909c0da8e
daishi <daishi@axlight.com>
2024-12-13 11:16:41 +0900

    fix vite loader

--- packages/waku/src/lib/utils/vite-loader.ts
+++ packages/waku/src/lib/utils/vite-loader.ts
@@ -1,15 +1,16 @@
 import { createServer as createViteServer } from 'vite';
+import type { ViteDevServer } from 'vite';
 import { fileURLToFilePath } from '../utils/path.js';
 
+let vite: ViteDevServer | undefined;
+
 export const loadServerFile = async (fileURL: string) => {
-  const vite = await createViteServer({
-    ssr: {
-      external: ['waku'],
-    },
-  });
-  try {
-    return vite.ssrLoadModule(fileURLToFilePath(fileURL));
-  } finally {
-    await vite.close();
+  if (!vite) {
+    vite = await createViteServer({
+      ssr: {
+        external: ['waku'],
+      },
+    });
   }
+  return vite.ssrLoadModule(fileURLToFilePath(fileURL));
 };
