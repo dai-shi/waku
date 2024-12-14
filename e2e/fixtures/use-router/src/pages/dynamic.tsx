d22713a36ac1d1b232ea4f6dac14b3570ed0e18c
daishi <daishi@axlight.com>
2024-12-14 15:30:28 +0900

    separate button

--- e2e/fixtures/use-router/src/pages/dynamic.tsx
+++ e2e/fixtures/use-router/src/pages/dynamic.tsx
@@ -1,22 +1,17 @@
-'use client';
-
-import { Link, useRouter_UNSTABLE } from 'waku';
+import { Link } from 'waku';
 import TestRouter from '../TestRouter.js';
 
-const Page = () => {
-  const router = useRouter_UNSTABLE();
-  return (
-    <>
-      <h1>Dynamic</h1>
-      <p>
-        <Link to="/static">Go to static</Link>
-        <button onClick={() => router.push(`/static`)}>
-          Static router.push button
-        </button>
-      </p>
-      <TestRouter />
-    </>
-  );
-};
+import { MyButton } from '../components/my-button.js';
+
+const Page = () => (
+  <>
+    <h1>Dynamic</h1>
+    <p>
+      <Link to="/static">Go to static</Link>
+      <MyButton />
+    </p>
+    <TestRouter />
+  </>
+);
 
 export default Page;
