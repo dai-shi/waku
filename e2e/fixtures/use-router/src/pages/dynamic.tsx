1f5a1fa5a03aaa39240a195d038a5e5c6d24ef6c
Tyler <26290074+thegitduck@users.noreply.github.com>
2024-12-13 21:12:40 -0800

    test(use-router): e2e test for router.push bug

--- e2e/fixtures/use-router/src/pages/dynamic.tsx
+++ e2e/fixtures/use-router/src/pages/dynamic.tsx
@@ -1,14 +1,22 @@
-import { Link } from 'waku';
+'use client';
+
+import { Link, useRouter_UNSTABLE } from 'waku';
 import TestRouter from '../TestRouter.js';
 
-const Page = () => (
-  <>
-    <h1>Dynamic</h1>
-    <p>
-      <Link to="/static">Go to static</Link>
-    </p>
-    <TestRouter />
-  </>
-);
+const Page = () => {
+  const router = useRouter_UNSTABLE();
+  return (
+    <>
+      <h1>Dynamic</h1>
+      <p>
+        <Link to="/static">Go to static</Link>
+        <button onClick={() => router.push(`/static`)}>
+          Static router.push button
+        </button>
+      </p>
+      <TestRouter />
+    </>
+  );
+};
 
 export default Page;
