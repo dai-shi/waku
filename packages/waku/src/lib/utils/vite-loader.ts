cd48f5fd8a429c755fb6a06bf5edcc38f2c8a838
daishi <daishi@axlight.com>
2024-12-25 10:27:27 +0900

    proper fix!

--- packages/waku/src/lib/utils/vite-loader.ts
+++ packages/waku/src/lib/utils/vite-loader.ts
@@ -1,4 +1,5 @@
 import { createServer as createViteServer } from 'vite';
+
 import { fileURLToFilePath } from '../utils/path.js';
 
 export const loadServerFile = async (fileURL: string) => {
@@ -6,13 +7,11 @@ export const loadServerFile = async (fileURL: string) => {
     ssr: {
       external: ['waku'],
     },
+    server: { middlewareMode: true, watch: null },
   });
   try {
     return vite.ssrLoadModule(fileURLToFilePath(fileURL));
   } finally {
-    // FIXME this is really a bad hack
-    setTimeout(() => {
-      vite.close().catch(() => {});
-    }, 1000);
+    await vite.ws.close();
   }
 };
