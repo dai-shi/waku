a7b256667bb9f0513949a68055ac7553ca844ea8
Tyler <26290074+tylersayshi@users.noreply.github.com>
2024-12-28 15:17:37 -0800

    fix: stylex example to use new vite plugin (#1105)
    fixes #750
    
    ---------
    
    Co-authored-by: Tyler <26290074+thegitduck@users.noreply.github.com>

--- examples/37_css/vite.config.ts
+++ examples/37_css/vite.config.ts
@@ -1,11 +1,11 @@
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
-import { stylexPlugin } from 'vite-plugin-stylex-dev';
+import stylexPlugin from 'vite-plugin-stylex';
 
 /** @type {import('vite').UserConfig} */
 export default {
   plugins: [
     {
-      name: 'hack-css-plugin-needed-for-stylex-dev-FIXME',
+      name: 'hack-css-plugin-needed-for-stylex-FIXME',
       resolveId(id: string) {
         if (id.endsWith('.css') && !id.endsWith('.vanilla.css')) {
           return id;
