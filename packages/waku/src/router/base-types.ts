23dec428150e9f1f013f34bdc2a66488dcfd5471
Daishi Kato <dai-shi@users.noreply.github.com>
2024-12-28 12:27:08 +0900

    chore: update dependencies (#1097)
    and eslint 9!

--- packages/waku/src/router/base-types.ts
+++ packages/waku/src/router/base-types.ts
@@ -10,12 +10,10 @@ export type {
   GetConfigResponse,
 } from './create-pages-utils/inferred-path-types.js';
 
-// eslint-disable-next-line @typescript-eslint/no-empty-object-type
 export interface RouteConfig {
   // routes to be overridden by users
 }
 
-// eslint-disable-next-line @typescript-eslint/no-empty-object-type
 export interface CreatePagesConfig {
   // routes to be overridden by users
 }
