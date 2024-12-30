f63f7944f20edce7b57960c2602d425e918ff1e0
daishi <daishi@axlight.com>
2024-12-30 09:00:42 +0900

    make the hack dev only

--- examples/37_css/waku.config.ts
+++ examples/37_css/waku.config.ts
@@ -2,18 +2,31 @@ import { defineConfig } from 'waku/config';
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 import { stylex } from 'vite-plugin-stylex-dev';
 
+// FIXME we would like to avoid this hack
+const hackCssPluginNeededForStylex = () => {
+  let mode = '';
+  return {
+    name: 'hack-css-plugin-needed-for-stylex',
+    configResolved(config: { mode: string }) {
+      mode = config.mode;
+    },
+    resolveId(id: string) {
+      if (
+        mode === 'development' &&
+        id.endsWith('.css') &&
+        !id.endsWith('.vanilla.css')
+      ) {
+        return id;
+      }
+    },
+  };
+};
+
 export default defineConfig({
   unstable_viteConfigs: {
     common: () => ({
       plugins: [
-        {
-          name: 'hack-css-plugin-needed-for-stylex-dev-FIXME',
-          resolveId(id: string) {
-            if (id.endsWith('.css') && !id.endsWith('.vanilla.css')) {
-              return id;
-            }
-          },
-        },
+        hackCssPluginNeededForStylex(),
         vanillaExtractPlugin(),
         stylex(),
       ],
