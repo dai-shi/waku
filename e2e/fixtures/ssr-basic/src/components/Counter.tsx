e254f6b8baf5c5db3e9c224c8c187962f72c89bc
daishi <daishi@axlight.com>
2024-12-31 09:45:53 +0900

    add failing test

--- e2e/fixtures/ssr-basic/src/components/Counter.tsx
+++ e2e/fixtures/ssr-basic/src/components/Counter.tsx
@@ -1,16 +1,21 @@
 'use client';
 
-import { useState } from 'react';
+import { useId, useState } from 'react';
 
 export const Counter = () => {
   const [count, setCount] = useState(0);
+  const id = useId();
   return (
     <div
       data-testid="counter"
       style={{ border: '3px blue dashed', margin: '1em', padding: '1em' }}
     >
       <p data-testid="count">{count}</p>
-      <button data-testid="increment" onClick={() => setCount((c) => c + 1)}>
+      <button
+        id={id}
+        data-testid="increment"
+        onClick={() => setCount((c) => c + 1)}
+      >
         Increment
       </button>
     </div>
