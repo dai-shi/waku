460c3ef2aeb58865f833bc69989721495894d254
Tyler <26290074+thegitduck@users.noreply.github.com>
2025-01-02 12:43:48 -0500

    feat(fs-router): api file support

--- packages/waku/src/router/fs-router.ts
+++ packages/waku/src/router/fs-router.ts
@@ -11,88 +11,101 @@ export function fsRouter(
   pages: string,
 ) {
   const platformObject = unstable_getPlatformObject();
-  return createPages(async ({ createPage, createLayout, createRoot }) => {
-    let files: string[] | undefined = platformObject.buildData
-      ?.fsRouterFiles as string[] | undefined;
-    if (!files) {
-      // dev and build only
-      const [{ readdir }, { join, dirname, extname, sep }, { fileURLToPath }] =
-        await Promise.all([
+  return createPages(
+    async ({ createPage, createLayout, createRoot, createApi }) => {
+      let files: string[] | undefined = platformObject.buildData
+        ?.fsRouterFiles as string[] | undefined;
+      if (!files) {
+        // dev and build only
+        const [
+          { readdir },
+          { join, dirname, extname, sep },
+          { fileURLToPath },
+        ] = await Promise.all([
           import(/* @vite-ignore */ DO_NOT_BUNDLE + 'node:fs/promises'),
           import(/* @vite-ignore */ DO_NOT_BUNDLE + 'node:path'),
           import(/* @vite-ignore */ DO_NOT_BUNDLE + 'node:url'),
         ]);
-      const pagesDir = join(dirname(fileURLToPath(importMetaUrl)), pages);
-      files = await readdir(pagesDir, {
-        encoding: 'utf8',
-        recursive: true,
-      });
-      files = files!.flatMap((file) => {
-        const myExt = extname(file);
-        const myExtIndex = EXTENSIONS.indexOf(myExt);
-        if (myExtIndex === -1) {
-          return [];
-        }
-        // HACK: replace "_slug_" to "[slug]" for build
-        file = file.replace(/(?<=^|\/|\\)_([^/]+)_(?=\/|\\|\.)/g, '[$1]');
-        // For Windows
-        file = sep === '/' ? file : file.replace(/\\/g, '/');
-        // HACK: resolve different extensions for build
-        const exts = [myExt, ...EXTENSIONS];
-        exts.splice(myExtIndex + 1, 1); // remove the second myExt
-        for (const ext of exts) {
-          const f = file.slice(0, -myExt.length) + ext;
-          if (loadPage(f)) {
-            return [f];
+        const pagesDir = join(dirname(fileURLToPath(importMetaUrl)), pages);
+        files = await readdir(pagesDir, {
+          encoding: 'utf8',
+          recursive: true,
+        });
+        files = files!.flatMap((file) => {
+          const myExt = extname(file);
+          const myExtIndex = EXTENSIONS.indexOf(myExt);
+          if (myExtIndex === -1) {
+            return [];
           }
-        }
-        throw new Error('Failed to resolve ' + file);
-      });
-    }
-    // build only - skip in dev
-    if (platformObject.buildOptions?.unstable_phase) {
-      platformObject.buildData ||= {};
-      platformObject.buildData.fsRouterFiles = files;
-    }
-    for (const file of files) {
-      const mod = await loadPage(file);
-      const config = await mod.getConfig?.();
-      const pathItems = file
-        .replace(/\.\w+$/, '')
-        .split('/')
-        .filter(Boolean);
-      const path =
-        '/' +
-        (['_layout', 'index', '_root'].includes(pathItems.at(-1)!)
-          ? pathItems.slice(0, -1)
-          : pathItems
-        ).join('/');
-      if (pathItems.at(-1) === '[path]') {
-        throw new Error(
-          'Page file cannot be named [path]. This will conflict with the path prop of the page component.',
-        );
-      } else if (pathItems.at(-1) === '_layout') {
-        createLayout({
-          path,
-          component: mod.default,
-          render: 'static',
-          ...config,
-        });
-      } else if (pathItems.at(-1) === '_root') {
-        createRoot({
-          component: mod.default,
-          render: 'static',
-          ...config,
-        });
-      } else {
-        createPage({
-          path,
-          component: mod.default,
-          render: 'dynamic',
-          ...config,
+          // HACK: replace "_slug_" to "[slug]" for build
+          file = file.replace(/(?<=^|\/|\\)_([^/]+)_(?=\/|\\|\.)/g, '[$1]');
+          // For Windows
+          file = sep === '/' ? file : file.replace(/\\/g, '/');
+          // HACK: resolve different extensions for build
+          const exts = [myExt, ...EXTENSIONS];
+          exts.splice(myExtIndex + 1, 1); // remove the second myExt
+          for (const ext of exts) {
+            const f = file.slice(0, -myExt.length) + ext;
+            if (loadPage(f)) {
+              return [f];
+            }
+          }
+          throw new Error('Failed to resolve ' + file);
         });
       }
-    }
-    return []; // TODO this type support for fsRouter pages
-  });
+      // build only - skip in dev
+      if (platformObject.buildOptions?.unstable_phase) {
+        platformObject.buildData ||= {};
+        platformObject.buildData.fsRouterFiles = files;
+      }
+      for (const file of files) {
+        const mod = await loadPage(file);
+        const config = await mod.getConfig?.();
+        const pathItems = file
+          .replace(/\.\w+$/, '')
+          .split('/')
+          .filter(Boolean);
+        const path =
+          '/' +
+          (['_layout', 'index', '_root'].includes(pathItems.at(-1)!)
+            ? pathItems.slice(0, -1)
+            : pathItems
+          ).join('/');
+        if (pathItems.at(-1) === '[path]') {
+          throw new Error(
+            'Page file cannot be named [path]. This will conflict with the path prop of the page component.',
+          );
+        } else if (pathItems.at(0) === 'api') {
+          createApi({
+            path: pathItems.slice(1).join('/'),
+            mode: config.mode ?? 'dynamic',
+            method: config.method ?? 'GET',
+            handler: mod.default,
+          });
+        } else if (pathItems.at(-1) === '_layout') {
+          createLayout({
+            path,
+            component: mod.default,
+            render: 'static',
+            ...config,
+          });
+        } else if (pathItems.at(-1) === '_root') {
+          createRoot({
+            component: mod.default,
+            render: 'static',
+            ...config,
+          });
+        } else {
+          createPage({
+            path,
+            component: mod.default,
+            render: 'dynamic',
+            ...config,
+          });
+        }
+      }
+      // HACK: to satisfy the return type, unused at runtime
+      return null as never;
+    },
+  );
 }
