4e892063894df4da034ee8a0538beacf839ef7ac
daishi <daishi@axlight.com>
2025-01-04 19:34:14 +0900

    reset error boundary

--- e2e/fixtures/ssr-catch-error/src/components/client-layout.tsx
+++ e2e/fixtures/ssr-catch-error/src/components/client-layout.tsx
@@ -1,9 +1,15 @@
 'use client';
 
+import { useEffect } from 'react';
 import type { ReactNode } from 'react';
 import { ErrorBoundary } from 'react-error-boundary';
+import type { FallbackProps } from 'react-error-boundary';
 
-const FallbackComponent = ({ error }: { error: any }) => {
+const FallbackComponent = ({ error, resetErrorBoundary }: FallbackProps) => {
+  useEffect(() => {
+    window.addEventListener('popstate', resetErrorBoundary);
+    return () => window.removeEventListener('popstate', resetErrorBoundary);
+  },[resetErrorBoundary])
   return (
     <div role="alert">
       <p>Unexpected error in client fallback</p>
