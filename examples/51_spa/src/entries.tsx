3e38eb77d833b8731ebf4bc488ba520a71f6b7c9
daishi <daishi@axlight.com>
2025-01-07 20:13:09 +0900

    fix lint

--- examples/51_spa/src/entries.tsx
+++ examples/51_spa/src/entries.tsx
@@ -1,7 +1,7 @@
 import { unstable_defineEntries as defineEntries } from 'waku/minimal/server';
 
 export default defineEntries({
-  handleRequest: async (input, { renderRsc, renderHtml }) => {
+  handleRequest: async (input, { renderRsc }) => {
     if (input.type === 'function') {
       const value = await input.fn(...input.args);
       return renderRsc({ _value: value });
