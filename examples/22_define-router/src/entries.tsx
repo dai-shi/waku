464a3e3f146ca7f3eb1f185efac6e648e267d010
Tyler <26290074+tylersayshi@users.noreply.github.com>
2025-01-08 04:08:14 -0800

    docs: examples for using api handling defineRouter and createPages (#1132)
    hello world api route handlers for defineRouter and createPages
    
    both seem to work nicely!
    
    ---------
    
    Co-authored-by: Tyler <26290074+thegitduck@users.noreply.github.com>

--- examples/22_define-router/src/entries.tsx
+++ examples/22_define-router/src/entries.tsx
@@ -7,6 +7,7 @@ import HomePage from './components/HomePage';
 import FooPage from './components/FooPage';
 import BarPage from './components/BarPage';
 import NestedBazPage from './components/NestedBazPage';
+import { readFile } from 'node:fs/promises';
 
 export default defineRouter({
   getRouteConfig: async () => {
@@ -207,10 +208,44 @@ export default defineRouter({
       ],
       isStatic: true,
     },
+    {
+      path: [
+        { type: 'literal', name: 'api' },
+        { type: 'literal', name: 'empty' },
+      ],
+    },
   ],
-  handleApi: async () => {
-    return {
-      status: 200,
-    };
+  handleApi: async (path) => {
+    if (path === '/api/hi.txt') {
+      const hiTxt = await readFile('./private/hi.txt');
+
+      return {
+        status: 200,
+        body: new ReadableStream({
+          start(controller) {
+            controller.enqueue(hiTxt);
+            controller.close();
+          },
+        }),
+      };
+    } else if (path === '/api/hi') {
+      return {
+        status: 200,
+        body: new ReadableStream({
+          start(controller) {
+            controller.enqueue(new TextEncoder().encode('hello world!'));
+            controller.close();
+          },
+        }),
+      };
+    } else if (path === '/api/empty') {
+      return {
+        status: 200,
+      };
+    } else {
+      return {
+        status: 404,
+      };
+    }
   },
 });
