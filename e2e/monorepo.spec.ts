a78a9367d62e024bda919b25446fe72a961fe5ff
daishi <daishi@axlight.com>
2025-01-17 17:37:54 +0900

    fix test 2

--- e2e/monorepo.spec.ts
+++ e2e/monorepo.spec.ts
@@ -23,7 +23,7 @@ for (const mode of ['DEV', 'PRD'] as const) {
       test('renders the home page', async ({ page }) => {
         await page.goto(`http://localhost:${port}`);
         await expect(page.getByTestId('header')).toHaveText('Waku');
-        // shows context value
+        // it should show context value from provider correctly
         await page.waitForSelector('[data-testid="context-consumer-mounted"]');
         await expect(page.getByTestId('context-consumer-value')).toHaveText(
           'provider value',
