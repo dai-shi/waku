cee6eee4645602509e6e98ce98d615f1a962f15d
daishi <daishi@axlight.com>
2025-01-23 11:12:27 +0900

    update dependencies and follow tailwindcss 4

--- examples/01_template/postcss.config.js
+++ examples/01_template/postcss.config.js
@@ -1,7 +1,7 @@
 /** @type {import('postcss-load-config').Config} */
 export default {
   plugins: {
-    tailwindcss: {},
+    '@tailwindcss/postcss': {},
     autoprefixer: {},
   },
 };
--- packages/website/postcss.config.js
+++ packages/website/postcss.config.js
@@ -1,8 +1,7 @@
 /** @type {import('postcss-load-config').Config} */
 export default {
   plugins: {
-    'tailwindcss/nesting': {},
-    tailwindcss: {},
+    '@tailwindcss/postcss': {},
     autoprefixer: {},
   },
 };
