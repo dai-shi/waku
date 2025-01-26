630c683459cc0ec67178c9d10c9b74ea8d43462a
Tyler <26290074+thegitduck@users.noreply.github.com>
2025-01-25 22:23:46 -0800

    add back compiler config

--- examples/05_compiler/waku.config.ts
+++ examples/05_compiler/waku.config.ts
@@ -1,8 +1,23 @@
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'waku/config';
+import react from '@vitejs/plugin-react';
+
+const ReactCompilerConfig = {};
+
+const getConfig = () => ({
+  plugins: [
+    react({
+      babel: {
+        plugins: [['babel-plugin-react-compiler', ReactCompilerConfig]],
+      },
+    }),
+  ],
+});
 
 export default defineConfig({
   unstable_viteConfigs: {
+    'dev-main': getConfig,
+    'build-client': getConfig,
     common: () => ({
       plugins: [tailwindcss()],
     }),
