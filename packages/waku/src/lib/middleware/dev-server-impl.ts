e08ef9e2ffe25ade62ff4f644e7d7aba6b7df9e6
daishi <daishi@axlight.com>
2025-02-13 23:24:02 +0900

    wip: add console.log 2

--- packages/waku/src/lib/middleware/dev-server-impl.ts
+++ packages/waku/src/lib/middleware/dev-server-impl.ts
@@ -141,6 +141,7 @@ const createMainViteServer = (
   const wakuDist = joinPath(fileURLToFilePath(import.meta.url), '../../..');
 
   const loadServerModuleMain = async (idOrFileURL: string) => {
+    console.log('======loadServerModuleMain', idOrFileURL);
     const vite = await vitePromise;
     if (!idOrFileURL.startsWith('file://')) {
       if (idOrFileURL === 'waku' || idOrFileURL.startsWith('waku/')) {
@@ -321,7 +322,6 @@ const createRscViteServer = (
     config: { rootDir: string; basePath: string },
     initialModules: ClonableModuleNode[],
   ) => {
-    console.log('--------------------resolveClientEntry', id);
     let file = id;
     if (file.startsWith('/@fs/')) {
       file = file.slice('/@fs'.length); // keep '/' at the beginning
