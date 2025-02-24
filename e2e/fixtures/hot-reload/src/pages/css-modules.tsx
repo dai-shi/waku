66bbcbc55a8baf42a5e3f49a3c5793f369f06a83
daishi <daishi@axlight.com>
2025-02-24 11:46:35 +0900

    fix lint and make tsconfig consistent

--- e2e/fixtures/hot-reload/src/pages/css-modules.tsx
+++ e2e/fixtures/hot-reload/src/pages/css-modules.tsx
@@ -3,7 +3,9 @@ import styles from './css-modules.module.css';
 export default async function CssModules() {
   return (
     <div>
-      <h1 data-testid="css-modules-header" className={styles.h1}>CSS Modules</h1>
+      <h1 data-testid="css-modules-header" className={styles.h1}>
+        CSS Modules
+      </h1>
     </div>
   );
 }
