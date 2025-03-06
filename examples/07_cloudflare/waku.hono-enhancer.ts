ddd5386337d37e995e3097400557faa48b7f242f
daishi <daishi@axlight.com>
2025-03-06 22:24:29 +0900

    fix waku.hono-enhancer

--- examples/07_cloudflare/waku.hono-enhancer.ts
+++ examples/07_cloudflare/waku.hono-enhancer.ts
@@ -1,28 +1,28 @@
 import type { Hono } from 'hono';
 
 const honoEnhancer = (createApp: (app: Hono) => Hono) => {
-  if (!import.meta.env || import.meta.env.PROD) {
-    return createApp;
-  }
-  const handlerPromise = import('./waku.cloudflare-dev-server').then(
-    ({ cloudflareDevServer }) =>
-      cloudflareDevServer({
-        // Optional config settings for the Cloudflare dev server (wrangler proxy)
-        // https://developers.cloudflare.com/workers/wrangler/api/#parameters-1
-        persist: {
-          path: '.wrangler/state/v3',
+  if (import.meta.env && !import.meta.env.PROD) {
+    const handlerPromise = import('./waku.cloudflare-dev-server').then(
+      ({ cloudflareDevServer }) =>
+        cloudflareDevServer({
+          // Optional config settings for the Cloudflare dev server (wrangler proxy)
+          // https://developers.cloudflare.com/workers/wrangler/api/#parameters-1
+          persist: {
+            path: '.wrangler/state/v3',
+          },
+        }),
+    );
+    return (appToCreate: Hono) => {
+      const app = createApp(appToCreate);
+      return {
+        fetch: async (req: Request) => {
+          const devHandler = await handlerPromise;
+          return devHandler(req, app);
         },
-      }),
-  );
-  return (appToCreate: Hono) => {
-    const app = createApp(appToCreate);
-    return {
-      fetch: async (req: Request) => {
-        const devHandler = await handlerPromise;
-        return devHandler(req, app);
-      },
+      };
     };
-  };
+  }
+  return createApp;
 };
 
 export default honoEnhancer;
